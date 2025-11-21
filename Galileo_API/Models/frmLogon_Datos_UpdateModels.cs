d3e7f57eedcdfb18904717596108dd755ef754e1
Franciny Suárez <150962001+fsuarez23@users.noreply.github.com>
2025-11-21 12:02:18 -0600

    Update frmLogon_Datos_UpdateModels.cs

--- Galileo_API/Models/frmLogon_Datos_UpdateModels.cs
+++ Galileo_API/Models/frmLogon_Datos_UpdateModels.cs
@@ -5,8 +5,7 @@ namespace Galileo.Models
     public class LogonUpdateData
     {
         public string usuario { get; set; } = string.Empty;
-        [Required]
-        public int id { get; set; }
+        public int? id { get; set; }
         public string email { get; set; } = string.Empty;
         public string tell_cell { get; set; } = string.Empty;
     }
