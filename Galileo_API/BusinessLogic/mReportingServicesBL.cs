a2499801aaca2cefb05830619a1693ddcdbe2260
Franciny Suárez <150962001+fsuarez23@users.noreply.github.com>
2025-11-25 12:23:25 -0600

    Reporting referencia

--- Galileo_API/BusinessLogic/mReportingServicesBL.cs
+++ Galileo_API/BusinessLogic/mReportingServicesBL.cs
@@ -1,34 +1,34 @@
-﻿// using Microsoft.AspNetCore.Mvc;
-// using Galileo.DataBaseTier;
-// using Galileo.Models;
-// using Galileo.Models.ERROR;
+﻿using Microsoft.AspNetCore.Mvc;
+using Galileo.DataBaseTier;
+using Galileo.Models;
+using Galileo.Models.ERROR;
 
-// namespace Galileo.BusinessLogic
-// {
-//     public class mReportingServicesBL
-//     {
-//         private readonly IConfiguration _config;
-//         private readonly mReportingServicesDB _reportingServicesDB;
+namespace Galileo.BusinessLogic
+{
+    public class mReportingServicesBL
+    {
+        private readonly IConfiguration _config;
+        private readonly mReportingServicesDB _reportingServicesDB;
 
-//         public mReportingServicesBL(IConfiguration config)
-//         {
-//             _config = config;
-//             _reportingServicesDB = new mReportingServicesDB(_config);
-//         }
+        public mReportingServicesBL(IConfiguration config)
+        {
+            _config = config;
+            _reportingServicesDB = new mReportingServicesDB(_config);
+        }
 
-//         public IActionResult ReporteRDLC_v2(frmReporteGlobal data)
-//         {
-//             return _reportingServicesDB.ReporteRDLC_v2(data);
-//         }
+        public IActionResult ReporteRDLC_v2(FrmReporteGlobal data)
+        {
+            return _reportingServicesDB.ReporteRDLC_v2(data);
+        }
 
-//         public ErrorDto<object> ReporteRDLC(frmReporteGlobal data)
-//         {
-//             return _reportingServicesDB.ReporteRDLC(data);
-//         }
-//         public ErrorDto<object> ReportesInfo(int CodEmpresa)
-//         {
-//             return _reportingServicesDB.ReportesInfo(CodEmpresa);
-//         }
+        public ErrorDto<object> ReporteRDLC(FrmReporteGlobal data)
+        {
+            return _reportingServicesDB.ReporteRDLC(data);
+        }
+        public ErrorDto<object> ReportesInfo(int CodEmpresa)
+        {
+            return _reportingServicesDB.ReportesInfo(CodEmpresa);
+        }
 
-//     }
-// }
+    }
+}
