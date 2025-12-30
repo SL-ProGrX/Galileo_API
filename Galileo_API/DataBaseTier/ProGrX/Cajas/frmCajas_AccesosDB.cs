6403ba11831c749cd9d74b912881a481ba268f3e
asaenzmpb <asaenz@mpbsystemlogic.com>
2025-12-30 10:44:37 -0600

    update frmCajas_Acceso

--- Galileo_API/DataBaseTier/ProGrX/Cajas/frmCajas_AccesosDB.cs
+++ Galileo_API/DataBaseTier/ProGrX/Cajas/frmCajas_AccesosDB.cs
@@ -21,17 +21,16 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
         /// <summary>
         /// Obtiene cajas disponibles
         /// </summary>
-        /// <param name="CodEmpresa"></param>
-        /// <param name="usuario"></param>
-        /// <returns></returns>
         public ErrorDto<List<DropDownListaGenericaModel>> Cajas_Apertura_Obtener(int CodEmpresa, string usuario)
         {
             string stringConn = new PortalDB(_config).ObtenerDbConnStringEmpresa(CodEmpresa);
+
             var response = new ErrorDto<List<DropDownListaGenericaModel>>
             {
                 Code = 0,
                 Result = new List<DropDownListaGenericaModel>()
             };
+
             try
             {
                 using var connection = new SqlConnection(stringConn);
@@ -43,12 +42,14 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
                 response.Description = ex.Message;
                 response.Result = null;
             }
+
             return response;
         }
 
         private List<DropDownListaGenericaModel> ObtenerCajasDisponibles(SqlConnection connection, string usuario)
         {
             var query = "spCajas_CierreCajasDisponibles";
+
             return connection.Query(query, new { Usuario = usuario }, commandType: CommandType.StoredProcedure)
                 .Select(row => new DropDownListaGenericaModel
                 {
@@ -60,29 +61,21 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
         /// <summary>
         /// Abre caja para el usuario
         /// </summary>
-        /// <param name="codEmpresa"></param>
-        /// <param name="codCaja"></param>
-        /// <param name="usuario"></param>
-        /// <param name="appVersion"></param>
-        /// <param name="clave"></param>
-        /// <returns></returns>
         public ErrorDto Cajas_AbreCaja(int codEmpresa, string codCaja, string usuario, string appVersion, string clave)
         {
             string stringConn = new PortalDB(_config).ObtenerDbConnStringEmpresa(codEmpresa);
-
-            string claveCifrada = FxStringCifrado(clave);
 
             var response = new ErrorDto
             {
                 Code = 0,
                 Description = "Ok",
-
             };
 
             try
             {
                 using var connection = new SqlConnection(stringConn);
 
+                var claveCifrada = MCajas.FxStringCifrado(clave);
 
                 var sqlValidar = @"
                             SELECT COUNT(*) 
@@ -105,8 +98,8 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
                     return response;
                 }
 
-
-                var result = connection.QueryFirstOrDefault<CajasAperturaDto>("spCajas_AbreCaja",
+                var result = connection.QueryFirstOrDefault<CajasAperturaDto>(
+                    "spCajas_AbreCaja",
                     new
                     {
                         Caja = codCaja,
@@ -120,9 +113,6 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
                     response.Description = "No existe Apertura Disponible para esta caja o se encuentra en uso por otro usuario.";
                     return response;
                 }
-
-                response.Description = "Ok";
-                response.Code = 0;
             }
             catch (Exception ex)
             {
@@ -132,65 +122,5 @@ namespace Galileo_API.DataBaseTier.ProGrX.Cajas
 
             return response;
         }
-
-
-
-        public static string FxStringCifrado(string input)
-        {
-            var vResBuilder = new System.Text.StringBuilder();
-            var vResXBuilder = new System.Text.StringBuilder();
-            int vSec = 0;
-
-            foreach (char c in input)
-            {
-                int ascii = (int)c;
-                vResBuilder.Insert(0, ascii.ToString());
-            }
-            string vRes = vResBuilder.ToString();
-            for (int i = 0; i < vRes.Length; i += 3)
-            {
-                int take = Math.Min(3, vRes.Length - i);
-                string slice = vRes.Substring(i, take);
-                int block = int.Parse(slice);
-                int transformed = block;
-
-                switch (vSec)
-                {
-                    case 0: transformed = block + 1; break;
-                    case 1: transformed = block - 5; break;
-                    case 2: transformed = block + 7; break;
-                    case 3: transformed = block - 13; break;
-                    case 4: transformed = block - 2; break;
-                    case 5: transformed = block + 3; break;
-                }
-
-                vResXBuilder.Append(transformed.ToString());
-                vSec = (vSec + 1) % 6; 
-            }
-
-            return FxDepuraCadena(vResXBuilder.ToString());
-        }
-
-        public static string FxDepuraCadena(string cadena)
-        {
-            var vResBuilder = new System.Text.StringBuilder();
-
-            for (int i = 0; i < cadena.Length - 1; i++)
-            {
-                string sub = cadena.Substring(i, 2);
-
-                if (int.TryParse(sub, out int num) && num > 31 && num != 39 && num != 34)
-                {
-                    vResBuilder.Insert(0, (char)num);
-                }
-            }
-
-            return vResBuilder.ToString();
-        }
-
-
-
     }
-
-
 }
---
+++ Galileo_API/DataBaseTier/mCajas.cs
@@ -0,0 +1,54 @@
+﻿namespace Galileo_API.DataBaseTier.ProGrX.Cajas
+{
+    public static class MCajas
+    {
+        public static string FxStringCifrado(string input)
+        {
+            var x = new System.Text.StringBuilder();
+            var y = new System.Text.StringBuilder();
+            int sec = 0;
+
+            foreach (var c in input)
+            {
+                x.Insert(0, ((int)c).ToString());
+            }
+
+            string s = x.ToString();
+            for (int i = 0; i < s.Length; i += 3)
+            {
+                int len = Math.Min(3, s.Length - i);
+                int b = int.Parse(s.Substring(i, len));
+
+                b += sec switch
+                {
+                    0 => 1,
+                    1 => -5,
+                    2 => 7,
+                    3 => -13,
+                    4 => -2,
+                    5 => 3,
+                    _ => 0
+                };
+
+                sec = (sec + 1) % 6;
+                y.Append(b.ToString());
+            }
+
+            return FxDepuraCadena(y.ToString());
+        }
+
+        private static string FxDepuraCadena(string cadena)
+        {
+            var res = new System.Text.StringBuilder();
+            for (int i = 0; i < cadena.Length - 1; i++)
+            {
+                if (int.TryParse(cadena.Substring(i, 2), out int n) &&
+                    n > 31 && n != 39 && n != 34)
+                {
+                    res.Insert(0, (char)n);
+                }
+            }
+            return res.ToString();
+        }
+    }
+}
