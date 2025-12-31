dc9be5f6030b88dd0a741311ef8772aaa1bf151c
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2025-12-30 18:20:46 -0600

    Update frmFNDSeguridadNivelesModels.cs

--- Galileo_API/Models/ProGrX/Fondos/frmFNDSeguridadNivelesModels.cs
+++ Galileo_API/Models/ProGrX/Fondos/frmFNDSeguridadNivelesModels.cs
@@ -4,10 +4,10 @@
     {
         public string cod_grupo { get; set; } = string.Empty;
         public string descripcion { get; set; } = string.Empty;
-        public decimal monto_inicio { get; set; }
-        public decimal monto_corte { get; set; }
-        public bool activo { get; set; }
-        public DateTime registro_fecha { get; set; }
+        public decimal? monto_inicio { get; set; }
+        public decimal? monto_corte { get; set; }
+        public bool? activo { get; set; }
+        public DateTime? registro_fecha { get; set; }
         public string registro_usuario { get; set; } = string.Empty;
     }
 
@@ -34,7 +34,7 @@
         public string cod_plan { get; set; } = string.Empty;
         public string cod_grupo { get; set; } = string.Empty;
         public string registro_usuario { get; set; } = string.Empty;
-        public bool asignar { get; set; }
+        public bool asignar { get; set; } = false;
     }
 
     public class FndSegNivelesUsuariosDto
@@ -42,6 +42,6 @@
         public string usuario { get; set; } = string.Empty;
         public string cod_grupo { get; set; } = string.Empty;
         public string registro_usuario { get; set; } = string.Empty;
-        public bool asignar { get; set; }
+        public bool asignar { get; set; } = false;
     }
 }
