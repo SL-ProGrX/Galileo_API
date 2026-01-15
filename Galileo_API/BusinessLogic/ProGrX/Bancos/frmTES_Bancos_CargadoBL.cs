bcd69b1736ba72d3856dea1982daaf74262b5107
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-01-14 18:55:56 -0600

    [DGM] migracion v1 a v2 Tes_Bancos_Cargados

---
+++ Galileo_API/BusinessLogic/ProGrX/Bancos/frmTES_Bancos_CargadoBL.cs
@@ -0,0 +1,66 @@
+using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Bancos;
+using Galileo_API.DataBaseTier.ProGrX.Bancos;
+using Newtonsoft.Json;
+
+namespace Galileo_API.BusinessLogic
+{
+    public class FrmTesBancosCargadoBL
+    {
+        private readonly FrmTesBancosCargadoDB _Db;
+
+        public FrmTesBancosCargadoBL(IConfiguration config)
+        {
+            _Db = new FrmTesBancosCargadoDB(config);
+        }
+
+        public ErrorDto<List<DropDownListaBancosCargados>> Tes_Bancos_Obtener(int CodEmpresa, string usuario)
+        {
+            return _Db.Tes_Bancos_Obtener(CodEmpresa, usuario);
+        }
+
+        public ErrorDto<List<TesBancoCargadoConceptos>> Tes_BancosCargadoConceptos_Obtener(int CodEmpresa, string? concepto = null)
+        {
+            return _Db.Tes_BancosCargadoConceptos_Obtener(CodEmpresa, concepto);
+        }
+
+        public ErrorDto<List<DropDownListaGenericaModel>> Tes_BancosCargadoCentroUnidades_Obtener(int CodEmpresa)
+        {
+            return _Db.Tes_BancosCargadoCentroUnidades_Obtener(CodEmpresa);
+        }
+
+        public ErrorDto<List<DropDownListaGenericaModel>> Tes_BancosCargadoCentroCostos_Obtener(int CodEmpresa)
+        {
+            return _Db.Tes_BancosCargadoCentroCostos_Obtener(CodEmpresa);
+        }
+
+        public ErrorDto<TesAutoRegistroLista> Tes_AutoRegistroLista_Obtener(int CodEmpresa, string filtros)
+        {
+            FiltrosLazyLoadData Jfiltros = JsonConvert.DeserializeObject<FiltrosLazyLoadData>(filtros) ?? new FiltrosLazyLoadData();
+            return _Db.Tes_AutoRegistroLista_Obtener(CodEmpresa, Jfiltros);
+        }
+
+        public ErrorDto TES_BancosCargados_Aplicar(int CodEmpresa, string cod_banco, string usuario, List<TesCargadoExcelDto> file)
+        {
+            return _Db.TES_BancosCargados_Aplicar(CodEmpresa, cod_banco, usuario, file);
+        }
+
+        public ErrorDto<List<TeslistaRegistroBancosDto>> TES_ListaRegistroBancos_Obtener(int CodEmpresa, string filtros)
+        {
+            return _Db.TES_ListaRegistroBancos_Obtener(CodEmpresa, filtros);
+        }
+
+
+        public ErrorDto TES_RegistrosBancosCargados_Aplicar(int CodEmpresa, string registroLista)
+        {
+            return _Db.TES_RegistrosBancosCargados_Aplicar(CodEmpresa, registroLista);
+        }
+
+        public ErrorDto TES_RegistrosBancosCargados_Elimina(int CodEmpresa, string registroLista)
+        {
+            return _Db.TES_RegistrosBancosCargados_Elimina(CodEmpresa, registroLista);
+        }
+
+    }
+}
---
+++ Galileo_API/DataBaseTier/ProGrX/Bancos/frmTES_Bancos_CargadoDB.cs
@@ -0,0 +1,406 @@
+using Dapper;
+using Galileo.DataBaseTier;
+using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Bancos;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace Galileo_API.DataBaseTier.ProGrX.Bancos
+{
+    public class FrmTesBancosCargadoDB
+    {
+        private readonly PortalDB _portalDB;
+
+
+        public FrmTesBancosCargadoDB(IConfiguration config)
+        {
+            _portalDB = new PortalDB(config);
+        }
+
+        /// <summary>
+        /// Obtiene la cuenta de los bancos
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaBancosCargados>> Tes_Bancos_Obtener(int CodEmpresa, string usuario)
+        {
+            return DbHelper.WithConn(_portalDB, CodEmpresa, conn =>
+            {
+                const string query = "exec spTes_Cuenta_Bancaria_Acceso @usuario, @TipoDoc, @Acceso";
+
+                return conn.Query<DropDownListaBancosCargados>(query, new { @Usuario = usuario, @TipoDoc = "DP", @Acceso = "SOL" }).ToList(); 
+            });
+        }
+
+
+        /// <summary>
+        /// Metodo para obtener los conceptos 
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<TesBancoCargadoConceptos>> Tes_BancosCargadoConceptos_Obtener(int CodEmpresa, string? concepto = null)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto<List<TesBancoCargadoConceptos>>
+            {
+                Code = 0,
+                Description = "Consulta realizada correctamente",
+                Result = new List<TesBancoCargadoConceptos>()
+            };
+
+            try
+            {
+                var conceptoTrim = concepto?.Trim();
+                var hasConcepto = !string.IsNullOrWhiteSpace(conceptoTrim);
+
+                const string sql = @"
+                            SELECT
+                                COD_CONCEPTO,
+                                DESCRIPCION,
+                                COD_CUENTA_MASK,
+                                DP_TRAMITE_APL,
+                                CUENTA_DESC
+                            FROM vTes_Conceptos
+                            WHERE AUTO_REGISTRO = 1
+                              AND ESTADO = 'A'
+                              AND (@concepto IS NULL OR COD_CONCEPTO = @concepto);";
+
+                response.Result = conn.Query<TesBancoCargadoConceptos>(
+                    sql,
+                    new { concepto = hasConcepto ? conceptoTrim : null }
+                ).ToList();
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+                response.Result = null;
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// Metodo para obtener las unidades asociadas 
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> Tes_BancosCargadoCentroUnidades_Obtener(int CodEmpresa)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto<List<DropDownListaGenericaModel>>
+            {
+                Code = 0,
+                Description = "Consulta realizada correctamente",
+                Result = new List<DropDownListaGenericaModel>()
+            };
+            try
+            {
+                var query = $@"select COD_UNIDAD as 'item', DESCRIPCION from vCNTX_UNIDADES_LOCAL";
+                response.Result = conn.Query<DropDownListaGenericaModel>(query).ToList();
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// Metodo para obtener los centros de costos 
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> Tes_BancosCargadoCentroCostos_Obtener(int CodEmpresa)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto<List<DropDownListaGenericaModel>>
+            {
+                Code = 0,
+                Description = "Consulta realizada correctamente",
+                Result = new List<DropDownListaGenericaModel>()
+            };
+            try
+            {
+                var query = $@"select COD_CENTRO_COSTO as 'item', DESCRIPCION from vCNTX_CENTRO_COSTO_LOCAL";
+                response.Result = conn.Query<DropDownListaGenericaModel>(query).ToList();
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+            return response;
+        }
+
+
+        /// <summary>
+        /// Metodo para obtener una lista de registros de auto registro de tesorer�a con paginaci�n y filtros
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="filtros"></param>
+        /// <returns></returns>
+        public ErrorDto<TesAutoRegistroLista> Tes_AutoRegistroLista_Obtener(int CodEmpresa, FiltrosLazyLoadData filtros)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var result = new ErrorDto<TesAutoRegistroLista>()
+            {
+                Code = 0,
+                Description = "Ok",
+                Result = new TesAutoRegistroLista()
+                {
+                    total = 0,
+                    lista = new List<TesAutoRegistroDto>()
+                }
+            };
+            try
+            {
+                var filtro = filtros?.filtro?.Trim();
+                var hasFiltro = !string.IsNullOrWhiteSpace(filtro);
+
+                
+                var offset = (filtros?.pagina).GetValueOrDefault(0);
+                var fetch = (filtros?.paginacion).GetValueOrDefault(0);
+                var usarPaginacion = fetch > 0;
+
+                var filtroLike = hasFiltro ? $"%{filtro}%" : null;
+
+                const string sqlCount = @"
+                    SELECT COUNT(1)
+                    FROM vTES_AUTO_REGISTRO
+                    WHERE
+                        (@filtro IS NULL)
+                     OR (CAST(id_auto AS NVARCHAR(50)) LIKE @filtroLike)
+                     OR (descripcion LIKE @filtroLike)
+                     OR (palabras_clave LIKE @filtroLike);";
+
+                result.Result.total = conn.QuerySingle<int>(sqlCount, new
+                {
+                    filtro = hasFiltro ? filtro : null,
+                    filtroLike
+                });
+
+                var sqlList = @"
+                    SELECT *
+                    FROM vTES_AUTO_REGISTRO
+                    WHERE
+                        (@filtro IS NULL)
+                     OR (CAST(id_auto AS NVARCHAR(50)) LIKE @filtroLike)
+                     OR (descripcion LIKE @filtroLike)
+                     OR (palabras_clave LIKE @filtroLike)
+                    ORDER BY id_auto ";
+
+                if (usarPaginacion)
+                {
+                    sqlList += @"
+                        OFFSET @offset ROWS
+                        FETCH NEXT @fetch ROWS ONLY;";
+                }
+
+                result.Result.lista = conn.Query<TesAutoRegistroDto>(sqlList, new
+                {
+                    filtro = hasFiltro ? filtro : null,
+                    filtroLike,
+                    offset,
+                    fetch
+                }).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = -1;
+                result.Description = ex.Message;
+                result.Result.total = 0;
+                result.Result.lista = new List<TesAutoRegistroDto>();
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Aplica el archivo de bancos cargado
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="cod_banco"></param>
+        /// <param name="usuario"></param>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public ErrorDto TES_BancosCargados_Aplicar(int CodEmpresa, string cod_banco, string usuario, List<TesCargadoExcelDto> file)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto
+            {
+                Code = 0,
+                Description = ""
+            };
+
+            try
+            {
+                
+                foreach (var row in file)
+                {
+
+                    var query = @"EXEC spTes_Bancos_Mov_Load @IdBanco, @Fecha, @Documento, @TipoMov, @Importe,@Descripcion";
+
+                    var result = conn.Query<int>(query, new
+                    {
+                        IdBanco = cod_banco,
+                        Fecha = row.fecha,
+                        Documento = row.documento,
+                        TipoMov = row.tipo,
+                        Importe = row.importe,
+                        Descripcion = row.descripcion,
+                    }).FirstOrDefault();
+
+                    if (result == -1)
+                    {
+                        response.Description += $"Documneto Repetido: [{row.documento}] \n";
+                    }
+                }
+
+                if(response.Description.Length > 0)
+                {
+                    response.Code = -1;
+                }
+                else
+                {
+                    response.Description = "Ok";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Obtiene los registros de bancos cargados pendientes
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="filtros"></param>
+        /// <returns></returns>
+        public ErrorDto<List<TeslistaRegistroBancosDto>> TES_ListaRegistroBancos_Obtener(int CodEmpresa, string filtros)
+        {
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            TesFiltrosRegistroBancoDto filtro = JsonConvert.DeserializeObject<TesFiltrosRegistroBancoDto>(filtros) ?? new TesFiltrosRegistroBancoDto();
+
+            var response = new ErrorDto<List<TeslistaRegistroBancosDto>>
+            {
+                Code = 0,
+                Result = new List<TeslistaRegistroBancosDto>()
+            };
+
+            try
+            {
+
+                string query = "spTes_Bancos_Mov_Consulta";
+
+                var parameters = new
+                {
+                    BancoId = filtro.cod_cuenta,
+                    Documento = filtro.ndocumento,
+                    Tipo = filtro.tipoMovimiento,
+                    FechaTipo = filtro.base_,
+                    FInicio = filtro.fechaInicio,
+                    FCorte = filtro.fechaCorte,
+                    MntInicio = filtro.montoInicio,
+                    MntCorte = filtro.montoCorte,
+                    Estado = filtro.estado,
+                    Descripcion = filtro.descripcion
+                };
+
+                response.Result = conn
+                    .Query<TeslistaRegistroBancosDto>(query, parameters, commandType: CommandType.StoredProcedure)
+                    .ToList();
+
+
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+                response.Result = null;
+            }
+
+            return response;
+        }
+
+
+        /// <summary>
+        /// Aplica el registro de bancos cargados
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="registroLista"></param>
+        /// <returns></returns>
+        public ErrorDto TES_RegistrosBancosCargados_Aplicar(int CodEmpresa, string registroLista)
+        {
+            List<RegistroBancoDto> lista = JsonConvert.DeserializeObject<List<RegistroBancoDto>>(registroLista) ?? new List<RegistroBancoDto>();
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto
+            {
+                Code = 0
+            };
+
+            try
+            {
+                var querySP = "";
+                foreach (var item in lista)
+                {
+                    querySP = "exec spTes_Bancos_Mov_Registro @LineaId, @Usuario, @AutoId, @Concepto, @Unidad, @Centro, @Cuenta";
+                    conn.Execute(querySP, new
+                    {
+                        LineaId = item.Linea_Id,
+                        item.Usuario,
+                        AutoId = item.Auto_Id,
+                        item.Concepto,
+                        item.Unidad,
+                        item.Centro,
+                        item.Cuenta
+                    });
+                }
+
+                response.Description = "Registro procesado correctamente!";
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+            return response;
+        }
+
+        public ErrorDto TES_RegistrosBancosCargados_Elimina(int CodEmpresa, string registroLista)
+        {
+            List<RegistroBancoDto> lista = JsonConvert.DeserializeObject<List<RegistroBancoDto>>(registroLista) ?? new List<RegistroBancoDto>();
+            using var conn = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+            var response = new ErrorDto
+            {
+                Code = 0
+            };
+
+            try
+            {
+                foreach (var item in lista)
+                {
+                    var querySP = "exec spTes_Bancos_Mov_Elimina @LineaId ";
+                    conn.Execute(querySP, new
+                    {
+                        LineaId = item.Linea_Id
+                    });
+                }
+                response.Description = "Registro procesado correctamente!";
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+            return response;
+        }
+    }
+}
