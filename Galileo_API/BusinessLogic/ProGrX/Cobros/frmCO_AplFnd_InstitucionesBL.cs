2760ba70296268e48cb45f9533a1a5b955161742
Marco Espinoza <mespinoza@mpbsystemlogic.com>
2026-01-20 19:32:12 -0600

    frmCO_AplFndInstituciones

---
+++ Galileo_API/BusinessLogic/ProGrX/Cobros/frmCO_AplFnd_InstitucionesBL.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Cobros;
+using Galileo.Models.ProGrX.Cobros;
+using Galileo.Models;
+
+namespace PgxAPI.BusinessLogic.ProGrX.Cobros
+{
+    public class FrmCOAplFndInstitucionesBL
+    {
+        private readonly FrmCOAplFndInstitucionesDB _db;
+
+        public FrmCOAplFndInstitucionesBL(IConfiguration config)
+        {
+            _db = new FrmCOAplFndInstitucionesDB(config);
+        }
+
+        public ErrorDto<CoAplFndInstitucionesListaResult> Co_AplFnd_Instituciones_Lista_Obtener(int CodEmpresa, string usuario, string jfiltros)
+        {
+
+            JsonConvert.DeserializeObject<FiltrosLazyLoadData>(jfiltros);
+
+            return _db.CoAplFndInstitucionesListaObtener(CodEmpresa, usuario);
+        }
+
+        public ErrorDto<CoAplFndInstitucionesListaResult> Co_AplFnd_Instituciones_Lista_Export(int CodEmpresa, string usuario, string jfiltros)
+        {
+            var filtros = JsonConvert.DeserializeObject<FiltrosLazyLoadData>(jfiltros) ?? new FiltrosLazyLoadData();
+            filtros.pagina = 0;
+            filtros.paginacion = 0;
+            return _db.CoAplFndInstitucionesListaObtener(CodEmpresa, usuario);
+        }
+
+        public ErrorDto Co_AplFnd_Instituciones_Actualizar(int CodEmpresa, CoAplFndInstitucionesActualizarRequest req)
+        {
+            return _db.Co_AplFnd_Instituciones_Actualizar(CodEmpresa, req);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Cobros/frmCO_AplFnd_InstitucionesModels.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Galileo.Models.ProGrX.Cobros
+{
+    public class CoAplFndInstitucionesData
+    {
+        public int cod_institucion { get; set; } = 0;
+        public string descripcion { get; set; } = "";
+        public string fecha_corte { get; set; } = "";
+        public bool aplica_pagos { get; set; } = false;
+        public bool isNew { get; set; } = false;   
+    }
+
+    public class CoAplFndInstitucionesListaResult
+    {
+        public int total { get; set; } = 0;
+        public List<CoAplFndInstitucionesData> lista { get; set; } = new();
+    }
+
+    public class CoAplFndInstitucionesActualizarRequest
+    {
+        public int cod_institucion { get; set; } = 0;
+        public bool aplica_pagos { get; set; } = false;
+        public string usuario_sesion { get; set; } = "";
+    }
+}
