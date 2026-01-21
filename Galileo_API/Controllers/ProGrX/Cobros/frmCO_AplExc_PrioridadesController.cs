de79fbec46c38d476014bf0a1f4b6397e1319624
Marco Espinoza <mespinoza@mpbsystemlogic.com>
2026-01-21 13:08:44 -0600

    frmCO_AplExcPrioridades

---
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_AplExc_PrioridadesController.cs
@@ -0,0 +1,56 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Cobros;
+using Galileo_API.BusinessLogic.ProGrX.Cobros;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PgxAPI.Controllers.ProGrX.Cobros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrmCOAplExcPrioridadesController : ControllerBase
+    {
+        private readonly FrmCOAplExcPrioridadesBL _bl;
+
+        public FrmCOAplExcPrioridadesController(IConfiguration config)
+        {
+            _bl = new FrmCOAplExcPrioridadesBL(config);
+        }
+
+        [Authorize]
+        [HttpGet("Co_AplExc_Prioridades_Lista_Obtener")]
+        public ErrorDto<COAplExcPrioridadesListaResult> Co_AplExc_Prioridades_Lista_Obtener(int CodEmpresa, string filtros)
+        {
+            return _bl.Co_AplExc_Prioridades_Lista_Obtener(CodEmpresa, filtros);
+        }
+
+        [Authorize]
+        [HttpGet("Co_AplExc_Prioridades_Lista_Export")]
+        public ErrorDto<COAplExcPrioridadesListaResult> Co_AplExc_Prioridades_Lista_Export(int CodEmpresa, string filtros)
+        {
+            return _bl.Co_AplExc_Prioridades_Lista_Export(CodEmpresa, filtros);
+        }
+
+        [Authorize]
+        [HttpPost("Co_AplExc_Prioridades_Guardar")]
+        public ErrorDto Co_AplExc_Prioridades_Guardar(int CodEmpresa, string usuario, [FromBody] COAplExcPrioridadData prioridad)
+        {
+            return _bl.Co_AplExc_Prioridades_Guardar(CodEmpresa, usuario, prioridad);
+        }
+
+        [Authorize]
+        [HttpDelete("Co_AplExc_Prioridades_Eliminar")]
+        public ErrorDto Co_AplExc_Prioridades_Eliminar(int CodEmpresa, string usuario, string codigo)
+        {
+            return _bl.Co_AplExc_Prioridades_Eliminar(CodEmpresa, usuario, codigo);
+        }
+
+        [Authorize]
+        [HttpGet("Co_AplExc_Prioridades_GarantiasDisponibles_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> Co_AplExc_Prioridades_GarantiasDisponibles_Obtener(int CodEmpresa)
+        {
+            return _bl.Co_AplExc_Prioridades_GarantiasDisponibles_Obtener(CodEmpresa);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Cobros/frmCO_AplExc_PrioridadesModels.cs
@@ -0,0 +1,25 @@
+﻿namespace Galileo.Models.ProGrX.Cobros
+{
+    public class COAplExcPrioridadData
+    {
+        public string codigo { get; set; } = "";
+        public string descripcion { get; set; } = "";
+        public int orden { get; set; } = 0;
+
+        public bool contrato_apl { get; set; } = false;
+        public bool activo { get; set; } = true;
+
+        public string registro_fecha { get; set; } = "";
+        public string registro_usuario { get; set; } = "";
+        public string modifica_fecha { get; set; } = "";
+        public string modifica_usuario { get; set; } = "";
+
+        public bool isNew { get; set; } = false;
+    }
+
+    public class COAplExcPrioridadesListaResult
+    {
+        public int total { get; set; } = 0;
+        public List<COAplExcPrioridadData> lista { get; set; } = new();
+    }
+}
