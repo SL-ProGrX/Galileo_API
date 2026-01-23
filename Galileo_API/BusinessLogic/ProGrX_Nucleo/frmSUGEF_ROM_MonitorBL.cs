706c86043da996225e879d5b4b1ee450f978775e
ccampos <ccampos@mpbsystemlogic.com>
2026-01-23 00:10:38 -0600

    frmSUGEF_ROM_Monitor

---
+++ Galileo_API/BusinessLogic/ProGrX_Nucleo/frmSUGEF_ROM_MonitorBL.cs
@@ -0,0 +1,26 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX_Nucleo;
+using Galileo_API.Models.ProGrX_Nucleo;
+
+namespace Galileo_API.BusinessLogic.ProGrX_Nucleo
+{
+    public class FrmSugefRomMonitorBL
+    {
+        private readonly FrmSugefRomMonitorDB _db;
+
+        public FrmSugefRomMonitorBL(IConfiguration config)
+        {
+            _db = new FrmSugefRomMonitorDB(config);
+        }
+
+        public ErrorDto<SugefTipoCambioResult?> SUGEF_TipoCambio_Obtener(int codEmpresa, DateTime fecha)
+        {
+            return _db.SUGEF_TipoCambio_Obtener(codEmpresa, fecha);
+        }
+
+        public ErrorDto<List<SugefRomMonitorConsultaResult>> SUGEF_ROM_Monitor_Consulta(int codEmpresa, DateTime corte)
+        {
+            return _db.SUGEF_ROM_Monitor_Consulta(codEmpresa, corte);
+        }
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX_Nucleo/frmSUGEF_ROM_MonitorController.cs
@@ -0,0 +1,34 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX_Nucleo;
+using Galileo_API.Models.ProGrX_Nucleo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX_Nucleo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrmSugefRomMonitorController : ControllerBase
+    {
+        private readonly FrmSugefRomMonitorBL _bl;
+
+        public FrmSugefRomMonitorController(IConfiguration config)
+        {
+            _bl = new FrmSugefRomMonitorBL(config);
+        }
+
+        [Authorize]
+        [HttpGet("SUGEF_TipoCambio_Obtener")]
+        public ErrorDto<SugefTipoCambioResult?> SUGEF_TipoCambio_Obtener(int codEmpresa, [FromQuery] DateTime fecha)
+        {
+            return _bl.SUGEF_TipoCambio_Obtener(codEmpresa, fecha);
+        }
+
+        [Authorize]
+        [HttpGet("SUGEF_ROM_Monitor_Consulta")]
+        public ErrorDto<List<SugefRomMonitorConsultaResult>> SUGEF_ROM_Monitor_Consulta(int codEmpresa, [FromQuery] DateTime corte)
+        {
+            return _bl.SUGEF_ROM_Monitor_Consulta(codEmpresa, corte);
+        }
+    }
+}
