7cf52a752ca4bc20f0c0966f905338023ddd8a44
ccampos <ccampos@mpbsystemlogic.com>
2026-01-23 20:14:17 -0600

    frmSUGEF_ROM_Monitor

--- Galileo_API/Models/ProGrX_Nucleo/frmSUGEF_ROM_MonitorModels.cs
+++ Galileo_API/Models/ProGrX_Nucleo/frmSUGEF_ROM_MonitorModels.cs
@@ -22,4 +22,51 @@
         public string? Cedula { get; set; }
         public string? Tipo_Id_Desc { get; set; }
     }
+
+    public class SugefRomMonitorDetalleResult
+    {
+        public string? TipoDoc { get; set; }
+        public string? NumDoc { get; set; }
+        public decimal? Monto_Col { get; set; }
+        public decimal? Monto_Dol { get; set; }
+        public string? Tipo_Doc_Desc { get; set; }
+        public string? Concepto_Desc { get; set; }
+        public DateTime? REGISTRO_FECHA { get; set; }
+        public string? REGISTRO_USUARIO { get; set; }
+    }
+
+    public class SugefRomMonitorFormaPagoResult
+    {
+        public DateTime Corte { get; set; }
+        public string? COD_LINEA { get; set; }
+        public string? TIPO_DOCUMENTO { get; set; }
+        public string? COD_TRANSACCION { get; set; }
+        public string? COD_FORMA_PAGO { get; set; }
+        public decimal? Monto_Col { get; set; }
+        public decimal? Monto_Dol { get; set; }
+        public string? Forma_Pago_Desc { get; set; }
+        public string? Concepto_Desc { get; set; }
+        public string? Origen_Recursos_Desc { get; set; }
+        public string? Pagador_Desc { get; set; }
+        public DateTime? REGISTRA_FECHA { get; set; }
+        public string? REGISTRA_USUARIO { get; set; }
+    }
+
+    public class SugefRomMonitorParams
+    {
+        public DateTime Corte { get; set; }
+        public decimal BaseDol { get; set; }
+        public string Usuario { get; set; } = string.Empty;
+    }
+
+    public class SugefRomMonitorFormaPagoActualizaParams
+    {
+        public int LineaId { get; set; }
+        public string TipoDoc { get; set; } = string.Empty;
+        public string NumDoc { get; set; } = string.Empty;
+        public string PagadorId { get; set; } = string.Empty;
+        public string Origen { get; set; } = string.Empty;
+        public string Notas { get; set; } = string.Empty;
+        public string Usuario { get; set; } = string.Empty;
+    }
 }
