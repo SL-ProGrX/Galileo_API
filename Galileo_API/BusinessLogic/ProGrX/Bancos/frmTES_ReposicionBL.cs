9a148b5a50eb76bfaf972be2c7d60215e00e06f1
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-01-25 10:56:46 -0600

    [DGM] frmTes_Reposicion

---
+++ Galileo_API/BusinessLogic/ProGrX/Bancos/frmTES_ReposicionBL.cs
@@ -0,0 +1,26 @@
+﻿using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Bancos;
+using Galileo_API.DataBaseTier.ProGrX.Bancos;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Bancos
+{
+    public class FrmTesReposicionBL
+    {
+        private readonly FrmTesReposicionDB _reposicionDb;
+
+        public FrmTesReposicionBL(IConfiguration config)
+        {
+            _reposicionDb = new FrmTesReposicionDB(config);
+        }
+
+        public ErrorDto<TesReposicionData> TES_Reposicion_Obtener(int CodEmpresa, int solicitud)
+        {
+            return _reposicionDb.TES_Reposicion_Obtenet(CodEmpresa, solicitud);
+        }
+
+        public ErrorDto TES_Reposicion_Guardar(int CodEmpresa, TesReposicionData solicitud)
+        {
+            return _reposicionDb.TES_Reposicion_Guardar(CodEmpresa, solicitud);
+        }
+    }
+}
