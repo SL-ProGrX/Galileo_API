2cf9d7b86472bc01bb1a0f7b9f09deca46e0716e
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-01-25 11:34:33 -0600

    [DGM] frmTes_TransferenciaReversa

---
+++ Galileo_API/BusinessLogic/ProGrX/Bancos/frmTES_TransferenciaReversaBL.cs
@@ -0,0 +1,59 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Bancos;
+using Galileo_API.DataBaseTier.ProGrX.Bancos;
+using Newtonsoft.Json;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Bancos
+{
+    public class FrmTesTransferenciaReversaBL
+    {
+        private readonly FrmTesTransferenciaReversaDB _TransferenciaReversaDB;
+
+        public FrmTesTransferenciaReversaBL(IConfiguration config)
+        {
+            _TransferenciaReversaDB = new FrmTesTransferenciaReversaDB(config);
+        }
+
+        public ErrorDto<List<DropDownListaGenericaModel>> sbTesBancoCargaCboAccesoGestion(int CodEmpresa, string usuario, string gestion)
+        {
+            return _TransferenciaReversaDB.sbTesBancoCargaCboAccesoGestion(CodEmpresa, usuario, gestion);
+        }
+
+        public ErrorDto<List<DropDownListaGenericaModel>> TES_ReversaPlanes_Obtener(int CodEmpresa, string id_banco)
+        {
+            return _TransferenciaReversaDB.TES_ReversaPlanes_Obtener(CodEmpresa, id_banco);
+        }
+
+        public ErrorDto<long> sbNTrasnferencia(int CodEmpresa, int id_banco, string tipo, string avance, string plan)
+        {
+            return _TransferenciaReversaDB.sbNTrasnferencia(CodEmpresa, id_banco, tipo, avance, plan);
+        }
+
+        public ErrorDto<List<TransferenciaSolicitudData>> TES_TransferenciaReversa_Obtener(int CodEmpresa, string solicitud)
+        {
+            var solicitaData = JsonConvert.DeserializeObject<TransferenciaSolicitudData>(solicitud);
+            return _TransferenciaReversaDB.TES_TransferenciaReversa_Obtener(CodEmpresa, solicitaData);
+        }
+
+        public ErrorDto TES_TransferenciaReversa_Aplicar(int CodEmpresa, TransferenciaReversaAplicaModel transferencia)
+        {
+            return _TransferenciaReversaDB.TES_TransferenciaReversa_Aplicar(CodEmpresa, transferencia);
+        }
+
+        public ErrorDto<List<TesReversionData>> TES_TransferenciaConsulta_Obtener(
+            int CodEmpresa,
+            int id_banco,
+            DateTime fechaInicio,
+            DateTime fechaFin)
+        {
+            return _TransferenciaReversaDB.TES_TransferenciaConsulta_Obtener(CodEmpresa, id_banco, fechaInicio, fechaFin);
+        }
+
+        public ErrorDto<List<TransferenciaDetalleModel>> TES_TransferenciaReversa_Detalle(int CodEmpresa, string id_reversion)
+        {
+            return _TransferenciaReversaDB.TES_TransferenciaReversa_Detalle(CodEmpresa, id_reversion);
+        }
+
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX/Bancos/frmTES_TransferenciaReversaController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Galileo_API.BusinessLogic.ProGrX.Bancos;
+using Galileo.Models.ERROR;
+using Galileo.Models;
+using Galileo.Models.ProGrX.Bancos;
+
+namespace Galileo_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmTesTransferenciaReversaController : ControllerBase
+    {
+        private readonly FrmTesTransferenciaReversaBL _TransferenciaReversaBL;
+
+        public FrmTesTransferenciaReversaController(IConfiguration config)
+        {
+            _TransferenciaReversaBL = new FrmTesTransferenciaReversaBL(config);
+        }
+
+        [HttpGet("Tes_BancoCargaCboAccesoGestion")]
+        public ErrorDto<List<DropDownListaGenericaModel>> sbTesBancoCargaCboAccesoGestion(int CodEmpresa, string usuario, string gestion)
+        {
+            return _TransferenciaReversaBL.sbTesBancoCargaCboAccesoGestion(CodEmpresa, usuario, gestion);
+        }
+
+        [HttpGet("TES_ReversaPlanes_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> TES_ReversaPlanes_Obtener(int CodEmpresa, string id_banco)
+        {
+            return _TransferenciaReversaBL.TES_ReversaPlanes_Obtener(CodEmpresa, id_banco);
+        }
+
+        [HttpGet("TES_sbNTrasnferencia_Obtener")]
+        public ErrorDto<long> TES_sbNTrasnferencia_Obtener(int CodEmpresa, int id_banco, string tipo, string avance, string plan)
+        {
+            return _TransferenciaReversaBL.sbNTrasnferencia(CodEmpresa, id_banco, tipo, avance, plan);
+        }
+
+        [HttpGet("TES_TransferenciaReversa_Obtener")]
+        public ErrorDto<List<TransferenciaSolicitudData>> TES_TransferenciaReversa_Obtener(int CodEmpresa, string solicitud)
+        {
+            return _TransferenciaReversaBL.TES_TransferenciaReversa_Obtener(CodEmpresa, solicitud);
+        }
+
+        [HttpPost("TES_TransferenciaReversa_Aplicar")]
+        public ErrorDto TES_TransferenciaReversa_Aplicar(int CodEmpresa, TransferenciaReversaAplicaModel transferencia)
+        {
+            return _TransferenciaReversaBL.TES_TransferenciaReversa_Aplicar(CodEmpresa, transferencia);
+        }
+
+        [HttpGet("TES_TransferenciaConsulta_Obtener")]
+        public ErrorDto<List<TesReversionData>> TES_TransferenciaConsulta_Obtener(
+            int CodEmpresa,
+            int id_banco,
+            DateTime fechaInicio,
+            DateTime fechaFin)
+        {
+            return _TransferenciaReversaBL.TES_TransferenciaConsulta_Obtener(CodEmpresa, id_banco, fechaInicio, fechaFin);
+        }
+
+        [HttpGet("TES_TransferenciaReversa_Detalle")]
+        public ErrorDto<List<TransferenciaDetalleModel>> TES_TransferenciaReversa_Detalle(int CodEmpresa, string id_reversion)
+        {
+            return _TransferenciaReversaBL.TES_TransferenciaReversa_Detalle(CodEmpresa, id_reversion);
+        }
+
+    }
+}
