b50c1613fcbd3437535850bcbae7fa100f3207b0
dizuniga <dzuniga@mpbsystemlogic.com>
2026-01-26 14:26:40 -0600

    frmCO_Incobrables_List_General

---
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_Incobrables_List_GeneralController.cs
@@ -0,0 +1,39 @@
+﻿using Galileo.Models.ERROR;
+using Galileo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc; 
+using Galileo_API.BusinessLogic.ProGrX.Cobros;
+using Galileo_API.Models.ProGrX.Cobros;
+using Galileo.Models.ProGrX.Cobros;
+
+namespace Galileo_API.Controllers.ProGrX.Cobros
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FrmCoIncobrablesListGeneralController : ControllerBase
+    {
+
+        private readonly FrmCoIncobrablesListGeneralBL _bl;
+
+        public FrmCoIncobrablesListGeneralController(IConfiguration config)
+            => _bl = new FrmCoIncobrablesListGeneralBL(config);
+
+
+
+        [Authorize]
+        [HttpGet("CoIncobrablesListMovimiento_Obtener")]
+        public ErrorDto<List<CbrIncobrableMovimientos>> CoIncobrablesListMovimiento_Obtener(int CodEmpresa, string pOperacion, string pCxC_Operacion)
+        {
+            return _bl.CoIncobrablesListMovimiento_Obtener(CodEmpresa, pOperacion, pCxC_Operacion);
+        }
+
+        [Authorize]
+        [HttpGet("CoIncobrablesListGeneral_Obtener")]
+        public ErrorDto<List<CbrIncobrableGeneral>> CoIncobrablesListGeneral_Obtener(int CodEmpresa, [FromQuery] CbrIncobrableFiltros filtros)
+        {
+            return _bl.CoIncobrablesListGeneral_Obtener(CodEmpresa,  filtros);
+        }
+    }
+}
---
+++ Galileo_API/DataBaseTier/ProGrX/Cobros/frmCO_Incobrables_List_GeneralDB.cs
@@ -0,0 +1,88 @@
+﻿using Dapper;
+using Galileo.DataBaseTier;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Bancos;
+using Galileo.Models.Security;
+using Galileo_API.Models.ProGrX.Cobros;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace Galileo_API.DataBaseTier.ProGrX.Cobros
+{
+    public class FrmCoIncobrablesListGeneralDb
+    {
+        private readonly PortalDB _portalDB;
+
+        public FrmCoIncobrablesListGeneralDb(IConfiguration config)
+        {
+            _portalDB = new PortalDB(config);
+        }
+
+
+        /// <summary>
+        /// Consulta de movimiento por operacion 
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="pOperacion"></param>
+        /// <param name="pCxC_Operacion"></param>
+        /// <returns></returns>
+        public ErrorDto<List<CbrIncobrableMovimientos>> CoIncobrablesListMovimiento_Obtener(int CodEmpresa, string pOperacion, string pCxC_Operacion)
+        {
+
+            return DbHelper.WithConn(_portalDB, CodEmpresa, conn =>
+            {
+                const string spName = "spCbr_Incobrable_Movimientos";
+
+                var cmd = new CommandDefinition(
+                    commandText: spName,
+                    parameters: new { pOperacion, pCxC_Operacion },
+                    commandType: CommandType.StoredProcedure,
+                    commandTimeout: 60  
+                );
+
+                return conn.Query<CbrIncobrableMovimientos>(cmd).ToList();
+            });
+                       
+                       
+        }
+
+
+        /// <summary>
+        /// Consulta de listados de casos incobrales
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <param name="filtros"></param>
+        /// <returns></returns>
+        public ErrorDto<List<CbrIncobrableGeneral>> CoIncobrablesListGeneral_Obtener(int CodEmpresa, CbrIncobrableFiltros filtros)
+        {
+
+            var inicio = DateTime.SpecifyKind(filtros.Inicio, DateTimeKind.Utc);
+            var corte = DateTime.SpecifyKind(filtros.Corte, DateTimeKind.Utc);
+            var auxiliar = DateTime.SpecifyKind(filtros.Auxiliar, DateTimeKind.Utc);
+
+            return DbHelper.WithConn(_portalDB, CodEmpresa, conn =>
+            {
+                const string spName = "spCbr_Incobrable_Lista_General";
+
+                var cmd = new CommandDefinition(
+                    commandText: spName,
+                    parameters: new
+                    {
+                        filtros.Estado,
+                        Inicio = inicio,
+                        Corte = corte,
+                        filtros.Filtro,
+                        Auxiliar = auxiliar,
+                        filtros.Usuario
+                    },
+                    commandType: CommandType.StoredProcedure,
+                    commandTimeout: 60 
+                );
+
+                return conn.Query<CbrIncobrableGeneral>(cmd).ToList();
+            });
+
+        }
+
+    }
+}
