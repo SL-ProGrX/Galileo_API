6035df7fa43e5655d60aee46f661d8a72b370857
ccampos <ccampos@mpbsystemlogic.com>
2026-01-30 00:07:28 -0600

    frmPolizas_Cat_Incendio_Causas

--- Galileo_API/Models/ProGrX_Polizas/frmPolizas_Cat_Incendio_CausasModels.cs
+++ Galileo_API/Models/ProGrX_Polizas/frmPolizas_Cat_Incendio_CausasModels.cs
@@ -10,21 +10,21 @@
     public class IncendioCausaSaveParams
     {
         public string Descripcion { get; set; } = string.Empty;
-        public bool Activo { get; set; }
+        public bool? Activo { get; set; }
         public string Usuario { get; set; } = string.Empty;
     }
 
     public class IncendioCausaUpdateParams
     {
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Descripcion { get; set; } = string.Empty;
-        public bool Activo { get; set; }
+        public bool? Activo { get; set; }
         public string Usuario { get; set; } = string.Empty;
     }
 
     public class IncendioCausaDeleteParams
     {
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Usuario { get; set; } = string.Empty;
     }
 }
