9adc396d0316ae2c4fa1a7e2a451c9789698ada3
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-02-04 14:56:16 -0600

    [DGM] frmCajas_IdentificaSF

---
+++ Galileo_API/Controllers/ProGrX/Cajas/frmCajas_IdentificaSFController.cs
@@ -0,0 +1,80 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Cajas;
+using Galileo_API.Models.ProGrX.Cajas;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Cajas
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCajasIdentificaSfController : ControllerBase
+    {
+        private readonly FrmCajasIdentificaSfBL _bl;
+        
+        public FrmCajasIdentificaSfController(IConfiguration config)
+        {
+            _bl = new FrmCajasIdentificaSfBL(config);
+        }
+
+        [HttpPost("Cajas_CajasIdentificaSf_Identificar")]
+        public ErrorDto Cajas_CajasIdentificaSf_Identificar(
+          int codEmpresa,
+          string cedula,
+          string nombre,
+          string usuario,
+          string pagadorId,
+          string origenRecursosId,
+          List<TesDepositoIdentificarDto> casos)
+        {
+            return _bl.Cajas_CajasIdentificaSf_Identificar(
+            codEmpresa,
+            cedula,
+            nombre,
+            usuario,
+            pagadorId,
+            origenRecursosId,
+            casos);
+        }
+
+        [HttpGet("Cajas_CajasIdentificaSf_Depositos_Obtener")]
+        public ErrorDto<List<FrmCajasIdentificaSfDepositoDto>> Cajas_CajasIdentificaSf_Depositos_Obtener(int CodEmpresa)
+        {
+            return _bl.Cajas_CajasIdentificaSf_Depositos_Obtener(CodEmpresa);
+        }
+
+        [HttpGet("Cajas_CajasIdentificaSf_Entidades_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> Cajas_CajasIdentificaSf_Entidades_Obtener(int CodEmpresa)
+        {
+            return _bl.Cajas_CajasIdentificaSf_Entidades_Obtener(CodEmpresa);
+        }
+
+        [HttpGet("Cajas_CajasIdentificaSf_Recursos_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> Cajas_CajasIdentificaSf_Recursos_Obtener(int CodEmpresa)
+        {
+            return _bl.Cajas_CajasIdentificaSf_Recursos_Obtener(CodEmpresa);
+        }
+
+        [HttpGet("Cajas_CajasIdentificaSf_Consultar")]
+        public ErrorDto<List<FrmCajasIdentificaSfTramitsRsdto>> Cajas_CajasIdentificaSf_Consultar(
+                int codEmpresa,
+                DateTime fechaInicio,
+                DateTime fechaCorte,
+                int bancoId,
+                decimal montoInicio,
+                decimal montoHasta,
+                string? numDocumento)
+        {
+            return _bl.Cajas_CajasIdentificaSf_Consultar(
+                codEmpresa,
+                fechaInicio,
+                fechaCorte,
+                bancoId,
+                montoInicio,
+                montoHasta,
+                numDocumento);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Cajas/frmCajas_IdentificaSFModels.cs
@@ -0,0 +1,32 @@
+﻿namespace Galileo_API.Models.ProGrX.Cajas
+{
+    public class TesDepositoIdentificarDto
+    {
+        public long DepositoId { get; set; }
+        public long BancoId { get; set; }
+        public string Documento { get; set; } = "";
+    }
+
+    public class FrmCajasIdentificaSfDepositoDto
+    {
+        public int id_banco { get; set; } = 0;
+        public string descripcion { get; set; } = "";
+        public string cta { get; set; } = "";
+        public string itmX { get; set; } = "";
+    }
+    public class FrmCajasIdentificaSfTramitsRsdto
+    {
+        public long dp_tramite_id { get; set; }
+        public string nsolicitud { get; set; } = string.Empty;
+        public int id_banco { get; set; }
+        public string bancodesc { get; set; } = string.Empty;
+        public string documento { get; set; } = string.Empty;
+        public DateTime fecha { get; set; }
+        public decimal monto { get; set; }
+        public string? descripcion { get; set; }
+        public DateTime? registro_fecha { get; set; }
+        public string? registro_usuario { get; set; }
+    }
+
+
+}
