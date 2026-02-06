cee72b9ec4fca4fdfe546daf7af7da6760a366c6
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-02-05 18:14:05 -0600

    Desarrollo de frmCO_AplFnd_Contratos_Aplicacion

---
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_AplFnd_Contratos_AplicacionController.cs
@@ -0,0 +1,33 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Cobros;
+using Galileo_API.Models.ProGrX.Cobros;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Cobros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCoAplFndContratosAplicacionController : ControllerBase
+    {
+        private readonly FrmCoAplFndContratosAplicacionBl _bl;
+
+        public FrmCoAplFndContratosAplicacionController(IConfiguration config) => _bl = new FrmCoAplFndContratosAplicacionBl(config);
+
+        
+        [HttpGet("CO_AplFndContrApl_Informacion_Obtener")]
+        public ErrorDto<List<CoAplFndContrAplInformacionData>> CO_AplFndContrApl_Informacion_Obtener(int codEmpresa)
+        {
+            return _bl.CO_AplFndContrApl_Informacion_Obtener(codEmpresa);
+        }
+
+        [HttpPost("CO_AplFndContrApl_Aplicar")]
+        public ErrorDto<CoAplExcProcesadosResult> CO_AplFndContrApl_Aplicar(int CodEmpresa, ExcedenteAplicarRequest request)
+        {
+            return _bl.CO_AplFndContrApl_Aplicar(CodEmpresa, request);
+        }
+    }
+}
+
+
