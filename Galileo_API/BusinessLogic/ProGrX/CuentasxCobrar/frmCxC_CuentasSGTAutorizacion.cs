8d3d2180e155485f60ddda1c301f9446bb784ee3
dizuniga <dzuniga@mpbsystemlogic.com>
2026-02-07 14:52:44 -0600

    frmCxC_CuentasSGTAutorizacion

---
+++ Galileo_API/BusinessLogic/ProGrX/CuentasxCobrar/frmCxC_CuentasSGTAutorizacion.cs
@@ -0,0 +1,27 @@
+﻿ 
+using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.CuentasxCobrar;
+using static Galileo_API.Models.ProGrX.CuentasxCobrar.FrmCxCCuentasSGTAutorizacionModels;
+
+
+namespace Galileo_API.BusinessLogic.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCCuentasSGTAutorizacionBL
+    {
+
+        private readonly FrmCxCCuentasSGTAutorizacionDB _db;
+
+        public FrmCxCCuentasSGTAutorizacionBL(IConfiguration config) => _db = new FrmCxCCuentasSGTAutorizacionDB(config);
+
+        public ErrorDto<CuentasSGTAutorizacionDto?> CxCCuentasSGTAutorizacion_Consulta(int codEmpresa, int operacion)
+        {
+             
+            return _db.CxCCuentasSGTAutorizacion_Consulta(codEmpresa, operacion);
+        }
+
+        public ErrorDto CxCCuentasSGTAutorizacion_Actualizar(int codEmpresa, string usuario, string estado, int operacion, string notas)
+        {
+            return _db.CxCCuentasSGTAutorizacion_Actualizar(codEmpresa, usuario, estado, operacion, notas);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/CuentasxCobrar/frmCxC_CuentasSGTAutorizacionModels.cs
@@ -0,0 +1,32 @@
+﻿namespace Galileo_API.Models.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCCuentasSGTAutorizacionModels
+    {
+        public class CuentasSGTAutorizacionDto
+        {
+            public int Operacion { get; set; } 
+            public string cod_concepto { get; set; } = string.Empty;
+            public string cedula { get; set; } = string.Empty;
+            public string nombre { get; set; } = string.Empty;
+            public decimal Monto { get; set; } 
+            public int? Dias_plazo { get; set; } 
+            public decimal? Tasa_Corriente { get; set; }
+            public decimal? cuota { get; set; }
+            public string cod_Contrato { get; set; } = string.Empty;
+            public string ContratoDesc { get; set; } = string.Empty;
+            public string ConceptoDesc { get; set; } = string.Empty;
+            public string Registro_Usuario { get; set; } = string.Empty;
+            public DateTime Registro_Fecha { get; set; }
+            public string Registro_FechaStr { get; set; } = string.Empty;
+            public string? Notas { get; set; }
+            public string? NotasDetalle { get; set; }
+        }
+
+        public class OperacionFacturasDto
+        {
+            public int Operacion { get; set; }
+            public string  cod_factura { get; set; }
+        }
+
+    }
+}
