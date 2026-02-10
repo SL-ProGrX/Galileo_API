6f6909dc779d7e9fc6fe4d83dc349565a92dc615
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-02-10 16:25:33 -0600

    ajuste

--- Galileo_API/Models/ProGrX_Contabilidad/frmCntX_UnidadesmModels.cs
+++ Galileo_API/Models/ProGrX_Contabilidad/frmCntX_UnidadesmModels.cs
@@ -5,13 +5,9 @@
         public string? cod_unidad { get; set; }
         public string? descripcion { get; set; }
         public int? nivel { get; set; }
-
-        // En VB6 eran Value 0/1
         public int? unidad_omision { get; set; }
         public int? reporta_renta { get; set; }
         public int? activa { get; set; }
-
-        // (Opcional) Si luego metés cuentas:
         public string? cta_renta { get; set; }
         public string? cta_renta_gasto { get; set; }
     }
@@ -20,11 +16,11 @@
     {
         public string cod_unidad { get; set; } = "";
         public string descripcion { get; set; } = "";
-        public int nivel { get; set; }
+        public int? nivel { get; set; }
 
-        public int unidad_omision { get; set; }
-        public int reporta_renta { get; set; }
-        public int activa { get; set; }
+        public int? unidad_omision { get; set; }
+        public int? reporta_renta { get; set; }
+        public int? activa { get; set; }
     }
 
     public class CntXUnidadEliminarDto
@@ -43,7 +39,6 @@
         public string? cod_centro_costo { get; set; }
         public string? descripcion { get; set; }
 
-        // true si existe registro en CntX_Unidades_CC
         public bool asignado { get; set; }
     }
 
@@ -51,6 +46,6 @@
     {
         public string cod_unidad { get; set; } = "";
         public string cod_centro_costo { get; set; } = "";
-        public int asociado { get; set; } // 1=insert, 0=delete
+        public int? asociado { get; set; } 
     }
 }
