c23f6633b085ab27b43b6849e988d4699af6dfd7
dizuniga <dzuniga@mpbsystemlogic.com>
2026-02-11 23:32:27 -0600

    frmCxC_ClientesContratos

---
+++ Galileo_API/BusinessLogic/ProGrX/CuentasxCobrar/frmCxC_ClientesContratosBL.cs
@@ -0,0 +1,63 @@
+﻿
+using Galileo.Models;
+using Galileo.Models.ERROR; 
+using Galileo_API.DataBaseTier.ProGrX.CuentasxCobrar;
+using static Galileo_API.Models.ProGrX.CuentasxCobrar.FrmCxCClientesContratosModels;
+
+
+namespace Galileo_API.BusinessLogic.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCClientesContratosBL
+    {
+
+        private readonly FrmCxCClientesContratosDb _db;
+
+        public FrmCxCClientesContratosBL(IConfiguration config) => _db = new FrmCxCClientesContratosDb(config);
+
+        public ErrorDto<string> CxCClientesPersonas_Contratos_Consultar(int codEmpresa, string cedula, int orden, string contrato)
+        {             
+            return _db.CxCClientesPersonas_Contratos_Consultar(codEmpresa, cedula,orden,contrato);
+        }
+        public ErrorDto<ClientesContratosData> CxCContratos_Consultar(int codEmpresa, string cedula, string contrato)
+        {
+            return _db.CxCContratos_Consultar(codEmpresa, cedula,contrato);
+        }
+        public ErrorDto CxCClientesPersonasContratos_Guardar(int codEmpresa, string usuario, ClientesContratosData datos)
+        {
+
+            return _db.CxCClientesPersonasContratos_Guardar(codEmpresa, usuario,datos);
+        }
+        public ErrorDto CxCClientesPersonas_Contratos_Eliminar(int codEmpresa, string usuario, string cedula, string contrato)
+        {
+            return _db.CxCClientesPersonas_Contratos_Eliminar(codEmpresa, usuario, cedula, contrato);
+        }
+        public ErrorDto CxCClientesPersonas_ContratosSuscripciones_Insertar(int CodEmpresa, string usuario, PersonasContratosSuscripcionesData datos)
+        {
+            return _db.CxCClientesPersonas_ContratosSuscripciones_Insertar(CodEmpresa, usuario, datos);
+        }
+        public ErrorDto CxCClientesPersonas_ContratosSuscripciones_Eliminar(int CodEmpresa, string usuario, string cedula, string contrato, string cargo)
+        {
+            return _db.CxCClientesPersonas_ContratosSuscripciones_Eliminar(CodEmpresa, usuario, cedula, contrato, cargo);
+        }
+        public ErrorDto CxCClientesPersonas_ContratosPagadores_Eliminar(int CodEmpresa, string usuario, string cedula, string contrato, string cedula_pagador)
+        {
+            return _db.CxCClientesPersonas_ContratosPagadores_Eliminar(CodEmpresa, usuario, cedula, contrato, cedula_pagador);
+        }
+        public ErrorDto CxCClientesPersonas_ContratosPagadores_Insertar(int CodEmpresa, string usuario, PersonasContratosPagadoresData datos)
+        {
+            return _db.CxCClientesPersonas_ContratosPagadores_Insertar(CodEmpresa, usuario, datos);
+        }
+        public ErrorDto<List<PersonasContratosPagadoresData>> CxCClientesPersonas_ContratosPagadores_Lista(int CodEmpresa, string cedula, string contrato)
+        {
+            return _db.CxCClientesPersonas_ContratosPagadores_Lista(CodEmpresa, cedula, contrato);
+        } 
+        public ErrorDto<List<PersonasContratosSuscripcionesData>> CxCClientesPersonas_ContratosSuscripcion_Lista(int CodEmpresa, string cedula, string contrato)
+        {
+            return _db.CxCClientesPersonas_ContratosSuscripcion_Lista(CodEmpresa, cedula, contrato);
+        }
+        public ErrorDto<List<DropDownListaGenericaModel>> CxC_Contratos_Obtener(int CodEmpresa)
+        {
+            return _db.CxC_Contratos_Obtener(CodEmpresa);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/CuentasxCobrar/frmCxC_ClientesContratosModels.cs
@@ -0,0 +1,58 @@
+﻿namespace Galileo_API.Models.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCClientesContratosModels
+    {
+       
+        public class ClientesContratosData
+        {
+            public string? Cod_Contrato { get; set; } = string.Empty;
+            public string? Descripcion { get; set; } = string.Empty;
+            public string? Notas { get; set; } = string.Empty;
+            public string? Contrato_Num { get; set; } = string.Empty;
+            public DateTime? Contrato_Vence { get; set; }
+            public string? Contrato_Tipo { get; set; } = string.Empty;
+            public int Plazo { get; set; }
+            public decimal Tasa_Corriente { get; set; } = 0;
+            public decimal Tasa_Mora { get; set; } = 0;
+            public string? Registro_Usuario { get; set; } = string.Empty;
+            public DateTime? Registro_fecha { get; set; }
+            public string? Actualiza_usuario { get; set; } = string.Empty;
+            public DateTime? Actualiza_fecha { get; set; }
+            public bool? Activo { get; set; }
+            public string? cedula { get; set; } = string.Empty;
+            public bool IsNew { get; set; }
+        }
+        public class PersonasContratosSuscripcionesData
+        {
+            public string? Cod_Contrato { get; set; } = string.Empty;
+            public string? Cedula { get; set; } = string.Empty;
+            public string? Descripcion { get; set; } = string.Empty;
+            public string? Cod_cargo { get; set; } = string.Empty;
+            public int Frecuencia_dias { get; set; }
+            public DateTime? Pago_ultimo { get; set; }
+            public DateTime? Pago_proximo { get; set; }
+            public decimal Recaudado { get; set; } = 0;
+            public string? Frecuencia_tipo { get; set; } = string.Empty;
+            public string? Tipo { get; set; } = string.Empty;
+            public decimal Valor { get; set; } = 0;
+            public int Modifica { get; set; }
+            public DateTime? Registro_fecha { get; set; }
+            public string? Contrato_tipo { get; set; } = string.Empty; 
+            public string? Registro_usuario { get; set; } = string.Empty;
+            public bool Activo { get; set; } = true;
+        }
+        public class PersonasContratosPagadoresData
+        {
+            public string? Cod_Contrato { get; set; } = string.Empty;
+            public string? Cedula { get; set; } = string.Empty;
+            public string? Nombre { get; set; } = string.Empty;
+            public string? Cedula_pagador { get; set; } = string.Empty;        
+            public string? Registro { get; set; } = string.Empty;
+            public bool Activo { get; set; } = true;
+            
+        }
+
+
+
+    }
+}
