090a764537588117da445a956fdf115657357f08
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-02-13 08:22:49 -0600

    frmPolizasAlertas_Parametros

---
+++ Galileo_API/Models/ProGrX_Polizas/frmPolizas_Alertas_ParametrosModels.cs
@@ -0,0 +1,42 @@
+﻿namespace Galileo_API.Models.ProGrX_Polizas
+{
+    public class PolAlertasParametrosGuardarDto
+    {
+        // Cliente envía esto
+        public string UnidadTiempo { get; set; } = "DAY"; // "DAY" | "HOUR" | "MINUTE"
+        public int AlertaRoja { get; set; }
+        public int AlertaAmarilla { get; set; }
+
+        public string? ContactoOficina { get; set; }
+        public string? ContactoTelefono { get; set; }
+        public string? ContactoEmail { get; set; }
+    }
+
+    public class PolAlertasParametrosDto
+    {
+        // Cliente recibe esto (para pintar pantalla)
+        public string UnidadTiempo { get; set; } = "";
+        public string UnidadTiempoEsp { get; set; } = ""; // "Días" | "Horas" | "Minutos"
+        public int AlertaRoja { get; set; }
+        public int AlertaAmarilla { get; set; }
+
+        public string? ContactoOficina { get; set; }
+        public string? ContactoTelefono { get; set; }
+        public string? ContactoEmail { get; set; }
+    }
+
+    public class PolAlertasEmailAgregarDto
+    {
+        // Cliente envía esto
+        public string Email { get; set; } = "";
+    }
+
+    public class PolAlertasEmailDto
+    {
+        // Cliente recibe esto
+        public int IdRegistro { get; set; }
+        public string Email { get; set; } = "";
+        public string? UsuarioInserta { get; set; }
+        public DateTime? FechaInserta { get; set; }
+    }
+}
