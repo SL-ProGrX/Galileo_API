95028201f93e7a02b01b17fc111fe210d10cb78e
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-02-13 09:00:29 -0600

    frmCO_ControlActualizaAbonos

---
+++ Galileo_API/DataBaseTier/ProGrX_Polizas/frmCR_Polizas_CambiosDB.cs
@@ -0,0 +1,14 @@
+﻿using Galileo.DataBaseTier;
+
+namespace Galileo_API.DataBaseTier.ProGrX_Polizas
+{
+    public class FrmCrPolizasCambiosDB
+    {
+        private readonly PortalDB _portalDB;
+
+        public FrmCrPolizasCambiosDB(IConfiguration config)
+        {
+            _portalDB = new PortalDB(config);
+        }
+    }
+}
