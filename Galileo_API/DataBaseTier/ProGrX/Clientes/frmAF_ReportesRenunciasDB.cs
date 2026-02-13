333fcfa4c9407eb068f5441eec55355b6b3955aa
ccampos <ccampos@mpbsystemlogic.com>
2026-02-13 17:42:57 -0600

    frmAF_ReportesRenuncias

---
+++ Galileo_API/DataBaseTier/ProGrX/Clientes/frmAF_ReportesRenunciasDB.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Galileo.DataBaseTier;
+using Galileo.Models.ERROR;
+using Galileo.Models; // Asegúrate de tener el namespace correcto para DropDownListaGenericaModel
+using System.Collections.Generic;
+
+namespace Galileo_API.DataBaseTier.ProGrX.Clientes
+{
+    public class FrmAFReportesRenunciasDB
+    {
+        private readonly PortalDB _portalDb;
+
+        public FrmAFReportesRenunciasDB(IConfiguration config)
+        {
+            _portalDb = new PortalDB(config);
+        }
+
+        /// <summary>
+        /// Consulta la lista de oficinas.
+        /// </summary>
+        /// <param name="codEmpresa">Código de la empresa.</param>
+        /// <returns>Lista de oficinas (item, descripcion).</returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> AfReportesRenunciasOficinas_Obtener(int codEmpresa)
+        {
+            var query = @"
+                SELECT cod_Oficina as item, rtrim(descripcion) as descripcion
+                FROM SIF_Oficinas
+                ORDER BY descripcion";
+            return DbHelper.ExecuteListQuery<DropDownListaGenericaModel>(_portalDb, codEmpresa, query);
+        }
+    }
+}
