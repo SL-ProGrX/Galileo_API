28e7f8521658bd52ed50510ba7bccb43d746db8c
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-02-13 06:07:26 -0600

    frmCO_ControlComGeneracion

---
+++ Galileo_API/DataBaseTier/ProGrX/Cobros/frmCO_ControlComGeneracionDB.cs
@@ -0,0 +1,33 @@
+﻿using Galileo.DataBaseTier;
+using Galileo.Models.ERROR;
+
+namespace Galileo_API.DataBaseTier.ProGrX.Cobros
+{
+    public class FrmCoControlComGeneracionDB
+    {
+        private readonly PortalDB _portalDB;
+
+        public FrmCoControlComGeneracionDB(IConfiguration config)
+        {
+            _portalDB = new PortalDB(config);
+        }
+
+        /// <summary>
+        /// Método para actualizar la comisión de generación. Ejecuta el procedimiento almacenado "spCbrComision_Actualiza" en la base de datos.
+        /// </summary>
+        /// <param name="CodEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto Co_ControlComGeneracion_Actualizar(int CodEmpresa)
+        {
+            try
+            {
+                return DbHelper.ExecuteNonQuery(_portalDB, CodEmpresa, "exec spCbrComision_Actualiza");
+            }
+            catch (Exception ex)
+            {
+                return DbHelper.ErrorResponse($"Error al actualizar comision: {ex.Message}");
+            }
+
+        }
+    }
+}
