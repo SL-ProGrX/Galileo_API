469a301a2342c85124708858a01b0739eafad5d8
ccampos <ccampos@mpbsystemlogic.com>
2026-02-14 11:55:15 -0600

    frmCxC_ContratosPagadores

---
+++ Galileo_API/BusinessLogic/ProGrX/CuentasxCobrar/frmCxC_ContratosPagadoresBL.cs
@@ -0,0 +1,31 @@
+﻿using Galileo_API.DataBaseTier.ProGrX.CuentasxCobrar;
+using Galileo_API.Models.ProGrX.CuentasxCobrar;
+using Galileo.Models.ERROR;
+
+namespace Galileo_API.BusinessLogic.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCContratosPagadoresBL
+    {
+        private readonly FrmCxCContratosPagadoresDB _db;
+
+        public FrmCxCContratosPagadoresBL(IConfiguration config)
+        {
+            _db = new FrmCxCContratosPagadoresDB(config);
+        }
+
+        public ErrorDto<List<CxcContratoPagadorDto>> CxcContratosPagadores_Lista(int codEmpresa, CxcContratoPagadorListaParams param)
+        {
+            return _db.CxcContratosPagadores_Lista(codEmpresa, param);
+        }
+
+        public ErrorDto<bool> CxcContratoPagador_Insertar(int codEmpresa, CxcContratoPagadorSaveParams param)
+        {
+            return _db.CxcContratoPagador_Insertar(codEmpresa, param);
+        }
+
+        public ErrorDto<bool> CxcContratoPagador_Eliminar(int codEmpresa, CxcContratoPagadorDeleteParams param)
+        {
+            return _db.CxcContratoPagador_Eliminar(codEmpresa, param);
+        }
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX/CuentasxCobrar/frmCxC_ContratosPagadoresController.cs
@@ -0,0 +1,42 @@
+﻿using Galileo_API.BusinessLogic.ProGrX.CuentasxCobrar;
+using Galileo_API.Models.ProGrX.CuentasxCobrar;
+using Galileo.Models.ERROR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Galileo_API.Controllers.ProGrX.CuentasxCobrar
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrmCxCContratosPagadoresController : ControllerBase
+    {
+        private readonly FrmCxCContratosPagadoresBL _bl;
+
+        public FrmCxCContratosPagadoresController(IConfiguration config)
+        {
+            _bl = new FrmCxCContratosPagadoresBL(config);
+        }
+
+        [Authorize]
+        [HttpPost("CxcContratosPagadores_Lista")]
+        public ErrorDto<List<CxcContratoPagadorDto>> CxcContratosPagadores_Lista(int codEmpresa, [FromBody] CxcContratoPagadorListaParams param)
+        {
+            return _bl.CxcContratosPagadores_Lista(codEmpresa, param);
+        }
+
+        [Authorize]
+        [HttpPost("CxcContratoPagador_Insertar")]
+        public ErrorDto<bool> CxcContratoPagador_Insertar(int codEmpresa, [FromBody] CxcContratoPagadorSaveParams param)
+        {
+            return _bl.CxcContratoPagador_Insertar(codEmpresa, param);
+        }
+
+        [Authorize]
+        [HttpPost("CxcContratoPagador_Eliminar")]
+        public ErrorDto<bool> CxcContratoPagador_Eliminar(int codEmpresa, [FromBody] CxcContratoPagadorDeleteParams param)
+        {
+            return _bl.CxcContratoPagador_Eliminar(codEmpresa, param);
+        }
+    }
+}
