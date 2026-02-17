3cb441e6a3ca2bf7c62def224b7d6c94d8ae02d6
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-02-16 18:41:05 -0600

    Desarrollo de frmCxC_CuentasAjustes

---
+++ Galileo_API/BusinessLogic/ProGrX/CuentasxCobrar/frmCxC_CuentasAjustesBL.cs
@@ -0,0 +1,43 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.CuentasxCobrar;
+using Galileo_API.Models.ProGrX.CuentasxCobrar;
+
+namespace Galileo_API.BusinessLogic.ProGrX.CuentasxCobrar
+{
+    public class FrmCxCCuentasAjustesBl
+    {
+        private readonly FrmCxCCuentasAjustesDb _db;
+
+        public FrmCxCCuentasAjustesBl(IConfiguration config) => _db = new FrmCxCCuentasAjustesDb(config);
+
+        public ErrorDto<CxCCuentasAjustesOperacionData> CxCCuentasAjustes_ConsultaOperacion_Obtener(int codEmpresa, int operacionId)
+        {
+            return _db.CxCCuentasAjustes_ConsultaOperacion_Obtener(codEmpresa, operacionId);
+        }
+
+        public ErrorDto<List<CxCCuentasAjustesCuotasData>> CxCCuentasAjustes_CuotasMora_Obtener(int codEmpresa, int operacionId)
+        {
+            return _db.CxCCuentasAjustes_CuotasMora_Obtener(codEmpresa, operacionId);
+        }
+
+        public ErrorDto<List<CxCCuentasAjustesCargosData>> CxCCuentasAjustes_Cargos_Obtener(int codEmpresa, int operacionId)
+        {
+            return _db.CxCCuentasAjustes_Cargos_Obtener(codEmpresa, operacionId);
+        }
+
+        public ErrorDto CxCCuentasAjustes_Fecha_Aplicar(int codEmpresa, CxCCuentasAjustesFechaRequest request)
+        {
+            return _db.CxCCuentasAjustes_Fecha_Aplicar(codEmpresa, request);
+        }
+
+        public ErrorDto CxCCuentasAjustes_CuotasMora_Eliminar(int codEmpresa, int operacionId, string usuario, List<CxCCuentasAjustesCuotasData> lista)
+        {
+            return _db.CxCCuentasAjustes_CuotasMora_Eliminar(codEmpresa, operacionId, usuario, lista);
+        }
+
+        public ErrorDto CxCCuentasAjustes_Cargos_Eliminar(int codEmpresa, int operacionId, string usuario, List<CxCCuentasAjustesCargosData> lista)
+        {
+            return _db.CxCCuentasAjustes_Cargos_Eliminar(codEmpresa, operacionId, usuario, lista);
+        }
+    }
+}
