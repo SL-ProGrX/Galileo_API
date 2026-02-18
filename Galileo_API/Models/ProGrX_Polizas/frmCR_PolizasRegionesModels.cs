b22272e2a2dd67e2b1401be626ea1b4f520a8977
David Gutierrez M <164664936+dgutierrezm90@users.noreply.github.com>
2026-02-18 15:16:06 -0600

    frmCR_Polizasregiones

---
+++ Galileo_API/Models/ProGrX_Polizas/frmCR_PolizasRegionesModels.cs
@@ -0,0 +1,23 @@
+﻿namespace Galileo_API.Models.ProGrX_Polizas
+{
+    public class CrdPolizasRegionDto
+    {
+        public int cod_region { get; set; }
+        public decimal monto_comercial { get; set; }
+        public decimal monto_personal { get; set; }
+
+        public DateTime? modifica_fecha { get; set; }
+        public string? modifica_usuario { get; set; }
+
+        public string registro_usuario { get; set; } = string.Empty;
+        public DateTime registro_fecha { get; set; }
+    }
+
+    public class CrdPolizasRegionGuardarDto
+    {
+        public string cod_poliza { get; set; } = string.Empty;
+        public int? cod_region { get; set; }            // null => insertar
+        public decimal? monto_comercial { get; set; }
+        public decimal? monto_personal { get; set; }
+    }
+}
