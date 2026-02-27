6b5424c3437286c41b76ff9526fa96bbe5bacdeb
ccampos <ccampos@mpbsystemlogic.com>
2026-02-26 18:48:56 -0600

    frmCntX_EF_Personal

---
+++ Galileo_API/BusinessLogic/ProGrX_Contabilidad/frmCntX_EF_PersonalBL.cs
@@ -0,0 +1,37 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX_Contabilidad;
+using Galileo_API.Models.ProGrX_Contabilidad;
+using System.Collections.Generic;
+
+namespace Galileo_API.BusinessLogic.ProGrX_Contabilidad
+{
+    public class FrmCntXEfPersonalBL
+    {
+        private readonly FrmCntXEfPersonalDB _db;
+
+        public FrmCntXEfPersonalBL(IConfiguration config)
+        {
+            _db = new FrmCntXEfPersonalDB(config);
+        }
+
+        public ErrorDto<List<CntXEfPersonalDto>> CntXEfPersonal_Lista(int codEmpresa, int codContabilidad)
+        {
+            return _db.CntXEfPersonal_Lista(codEmpresa, codContabilidad);
+        }
+
+        public ErrorDto<bool> CntXEfPersonal_Guardar(int codEmpresa, string registroUsuario, CntXEfPersonalSaveParams param)
+        {
+            return _db.CntXEfPersonal_Guardar(codEmpresa, registroUsuario, param);
+        }
+
+        public ErrorDto<bool> CntXEfPersonal_Eliminar(int codEmpresa, string registroUsuario, CntXEfPersonalDeleteParams param)
+        {
+            return _db.CntXEfPersonal_Eliminar(codEmpresa, registroUsuario, param);
+        }
+
+        public ErrorDto<List<CntXEfSeccionDto>> CntXEfSecciones_Lista(int codEmpresa, int codContabilidad, string codEf)
+        {
+            return _db.CntXEfSecciones_Lista(codEmpresa, codContabilidad, codEf);
+        }
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX_Contabilidad/frmCntX_EF_PersonalController.cs
@@ -0,0 +1,55 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX_Contabilidad;
+using Galileo_API.Models.ProGrX_Contabilidad;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Galileo_API.Controllers.ProGrX_Contabilidad
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FrmCntXEfPersonalController : ControllerBase
+    {
+        private readonly FrmCntXEfPersonalBL _bl;
+
+        public FrmCntXEfPersonalController(IConfiguration config)
+        {
+            _bl = new FrmCntXEfPersonalBL(config);
+        }
+
+        [Authorize]
+        [HttpGet("CntXEfPersonal_Lista")]
+        public ErrorDto<List<CntXEfPersonalDto>> CntXEfPersonal_Lista(int codEmpresa, int codContabilidad)
+        {
+            return _bl.CntXEfPersonal_Lista(codEmpresa, codContabilidad);
+        }
+
+        [Authorize]
+        [HttpPost("CntXEfPersonal_Guardar")]
+        public ErrorDto<bool> CntXEfPersonal_Guardar(int codEmpresa, string registroUsuario, [FromBody] CntXEfPersonalSaveParams param)
+        {
+            if (string.IsNullOrWhiteSpace(registroUsuario))
+                return new ErrorDto<bool> { Code = -1, Description = "Usuario requerido", Result = false };
+
+            return _bl.CntXEfPersonal_Guardar(codEmpresa, registroUsuario, param);
+        }
+
+        [Authorize]
+        [HttpPost("CntXEfPersonal_Eliminar")]
+        public ErrorDto<bool> CntXEfPersonal_Eliminar(int codEmpresa, string registroUsuario, [FromBody] CntXEfPersonalDeleteParams param)
+        {
+            if (string.IsNullOrWhiteSpace(registroUsuario))
+                return new ErrorDto<bool> { Code = -1, Description = "Usuario requerido", Result = false };
+
+            return _bl.CntXEfPersonal_Eliminar(codEmpresa, registroUsuario, param);
+        }
+
+        [Authorize]
+        [HttpGet("CntXEfSecciones_Lista")]
+        public ErrorDto<List<CntXEfSeccionDto>> CntXEfSecciones_Lista(int codEmpresa, int codContabilidad, string codEf)
+        {
+            return _bl.CntXEfSecciones_Lista(codEmpresa, codContabilidad, codEf);
+        }
+    }
+}
