018e4b1b41b9f86d9b8a175e51579f0b6f3195db
ccampos <ccampos@mpbsystemlogic.com>
2026-03-13 16:06:11 -0600

    frmCntX_ERCuentas

---
+++ Galileo_API/Models/ProGrX_Contabilidad/frmCntX_ERCuentasModels.cs
@@ -0,0 +1,30 @@
+﻿namespace Galileo_API.Models.ProGrX_Contabilidad
+{
+    public class CntXInvPeriodicoDto
+    {
+        public int Anio { get; set; }
+        public int Mes { get; set; }
+        public string Cod_Cuenta { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Saldo_Final { get; set; }
+    }
+
+    public class CntXInvPeriodicoSaveParams
+    {
+        public required int CodContabilidad { get; set; }
+        public required int Anio { get; set; }
+        public required int Mes { get; set; }
+        public string CodCuenta { get; set; } = string.Empty;
+        public decimal SaldoFinal { get; set; }
+        public string RegistroUsuario { get; set; } = string.Empty;
+    }
+
+    public class CntXInvPeriodicoDeleteParams
+    {
+        public required int CodContabilidad { get; set; }
+        public required int Anio { get; set; }
+        public required int Mes { get; set; }
+        public string CodCuenta { get; set; } = string.Empty;
+        public string RegistroUsuario { get; set; } = string.Empty;
+    }
+}
