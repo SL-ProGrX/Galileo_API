c6d4cea14ec5c34b1c074ea8a770830297ca231f
ccampos <ccampos@mpbsystemlogic.com>
2026-03-15 09:03:47 -0600

    frmCntX_PlantillaAsientosGenera

---
+++ Galileo_API/BusinessLogic/ProGrX_Contabilidad/frmCntX_PlantillaAsientosGeneraBL.cs
@@ -0,0 +1,38 @@
+﻿using Galileo_API.DataBaseTier.ProGrX_Contabilidad;
+using Galileo_API.Models.ProGrX_Contabilidad;
+using Galileo.Models.ERROR;
+using System.Collections.Generic;
+
+namespace Galileo_API.BusinessLogic.ProGrX_Contabilidad
+{
+    public class FrmCntXPlantillaAsientosGeneraBL
+    {
+        private readonly FrmCntXPlantillaAsientosGeneraDB _db;
+
+        public FrmCntXPlantillaAsientosGeneraBL(IConfiguration config)
+        {
+            _db = new FrmCntXPlantillaAsientosGeneraDB(config);
+        }
+
+        public ErrorDto<List<CntXPlantillaAsientosDto>> CntXPlantillaAsientos_Lista(int codEmpresa, int codContabilidad)
+            => _db.CntXPlantillaAsientos_Lista(codEmpresa, codContabilidad);
+
+        public ErrorDto<CntXPlantillaAsientosDto?> CntXPlantillaAsientos_Get(int codEmpresa, int codContabilidad, string codPlantilla)
+            => _db.CntXPlantillaAsientos_Get(codEmpresa, codContabilidad, codPlantilla);
+
+        public ErrorDto<bool> CntXPlantillaAsientos_UpdateConsecutivo(int codEmpresa, CntXPlantillaAsientosUpdateParams param)
+            => _db.CntXPlantillaAsientos_UpdateConsecutivo(codEmpresa, param);
+
+        public ErrorDto<bool> CntxAsientos_Insert(int codEmpresa, CntxAsientosInsertParams param)
+            => _db.CntxAsientos_Insert(codEmpresa, param);
+
+        public ErrorDto<List<CntXPlantillaDetalleDto>> CntXPlantillaDetalle_Lista(int codEmpresa, int codContabilidad, string codPlantilla)
+            => _db.CntXPlantillaDetalle_Lista(codEmpresa, codContabilidad, codPlantilla);
+
+        public ErrorDto<bool> CntxAsientosDetalle_Insert(int codEmpresa, CntxAsientosDetalleInsertParams param)
+            => _db.CntxAsientosDetalle_Insert(codEmpresa, param);
+
+        public ErrorDto<int> CntXPeriodos_ExisteAbierto(int codEmpresa, int codContabilidad, int anio, int mes)
+             => _db.CntXPeriodos_ExisteAbierto(codEmpresa, codContabilidad, anio, mes);
+    }
+}
---
+++ Galileo_API/Models/ProGrX_Contabilidad/frmCntX_PlantillaAsientosGeneraModels.cs
@@ -0,0 +1,67 @@
+﻿namespace Galileo_API.Models.ProGrX_Contabilidad
+{
+    public class CntXPlantillaAsientosDto
+    {
+        public string Cod_Plantilla { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int Consecutivo { get; set; }
+        public string Anio_Inicio { get; set; } = string.Empty;
+        public string Mes_Inicio { get; set; } = string.Empty;
+        public string Tipo_Asiento { get; set; } = string.Empty;
+        public string Asiento_Descripcion { get; set; } = string.Empty;
+        public string Asiento_Documento { get; set; } = string.Empty;
+        public string Asiento_Detalle { get; set; } = string.Empty;
+    }
+
+    public class CntXPlantillaAsientosUpdateParams
+    {
+        public required int CodContabilidad { get; set; }
+        public string CodPlantilla { get; set; } = string.Empty;
+        public int Consecutivo { get; set; }
+    }
+
+    public class CntxAsientosInsertParams
+    {
+        public required int CodContabilidad { get; set; }
+        public string TipoAsiento { get; set; } = string.Empty;
+        public string NumAsiento { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string FechaAsiento { get; set; } = string.Empty;
+        public int Anio { get; set; }
+        public int Mes { get; set; }
+        public string Usuario { get; set; } = string.Empty;
+        public string Notas { get; set; } = string.Empty;
+    }
+
+    public class CntXPlantillaDetalleDto
+    {
+        public string Cod_Plantilla { get; set; } = string.Empty;
+        public int Num_Linea { get; set; }
+        public string Cod_Cuenta { get; set; } = string.Empty;
+        public decimal Monto_Debito { get; set; }
+        public decimal Monto_Credito { get; set; }
+        public string Documento { get; set; } = string.Empty;
+        public string Detalle { get; set; } = string.Empty;
+        public string Cod_Unidad { get; set; } = string.Empty;
+        public string Cod_Divisa { get; set; } = string.Empty;
+        public decimal Tipo_Cambio { get; set; }
+        public string Cod_Centro_Costo { get; set; } = string.Empty;
+    }
+
+    public class CntxAsientosDetalleInsertParams
+    {
+        public required int CodContabilidad { get; set; }
+        public string TipoAsiento { get; set; } = string.Empty;
+        public string NumAsiento { get; set; } = string.Empty;
+        public string CodCuenta { get; set; } = string.Empty;
+        public decimal MontoDebito { get; set; }
+        public decimal MontoCredito { get; set; }
+        public string Documento { get; set; } = string.Empty;
+        public string Detalle { get; set; } = string.Empty;
+        public int NumLinea { get; set; }
+        public string CodUnidad { get; set; } = string.Empty;
+        public string CodDivisa { get; set; } = string.Empty;
+        public decimal TipoCambio { get; set; }
+        public string CodCentroCosto { get; set; } = string.Empty;
+    }
+}
