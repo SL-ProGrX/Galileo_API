af6c5884d66777de0934b4aa1f92273ec58d6339
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-03-20 16:37:48 -0600

    Desarollo frmCntX_ERConfiguracion

---
+++ Galileo_API/Models/ProGrX_Contabilidad/frmCntX_ERConfiguracionModels.cs
@@ -0,0 +1,15 @@
+﻿namespace Galileo_API.Models.ProGrX_Contabilidad
+{
+
+    public class CntxTipoCuentaERDto
+    {
+        public string? tipo_cuenta { get; set; }
+        public string? descripcion { get; set; }
+        public string? er { get; set; }
+    }
+
+
+}
+
+
+
