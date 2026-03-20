efb4a70988a73c12565f57dc627359a57bfa0722
ccampos <ccampos@mpbsystemlogic.com>
2026-03-20 15:44:25 -0600

    frmCntX_Roles_Acceso

--- Galileo_API/Controllers/ProGrX_Contabilidad/frmCntX_Roles_AccesoController.cs
+++ Galileo_API/Controllers/ProGrX_Contabilidad/frmCntX_Roles_AccesoController.cs
@@ -59,5 +59,38 @@ namespace Galileo_API.Controllers.ProGrX_Contabilidad
             [FromQuery] string usuario,
             [FromQuery] string filtro = "")
             => _bl.CntXAcCentroCosto_Consulta(codEmpresa, codContabilidad, rol, unidad, filtro, usuario);
+
+        [HttpGet("CntXAcMiembros_Consulta")]
+        public ActionResult<ErrorDto<List<CntXAcMiembroDto>>> CntXAcMiembros_Consulta(
+            [FromQuery] int codEmpresa,
+            [FromQuery] int codContabilidad,
+            [FromQuery] string rol,
+            [FromQuery] string usuario,
+            [FromQuery] string filtro = "")
+            => _bl.CntXAcMiembros_Consulta(codEmpresa, codContabilidad, rol, filtro, usuario);
+
+        [HttpPost("CntXAcCuentas_Asigna")]
+        public ActionResult<ErrorDto<bool>> CntXAcCuentas_Asigna([FromQuery] int codEmpresa, [FromBody] CntXAcCuentaAsignaParams param)
+            => _bl.CntXAcCuentas_Asigna(codEmpresa, param);
+
+        [HttpPost("CntXAcUnidades_Asigna")]
+        public ActionResult<ErrorDto<bool>> CntXAcUnidades_Asigna([FromQuery] int codEmpresa, [FromBody] CntXAcUnidadAsignaParams param)
+            => _bl.CntXAcUnidades_Asigna(codEmpresa, param);
+
+        [HttpPost("CntXAcCentroCosto_Asigna")]
+        public ActionResult<ErrorDto<bool>> CntXAcCentroCosto_Asigna([FromQuery] int codEmpresa, [FromBody] CntXAcCentroCostoAsignaParams param)
+            => _bl.CntXAcCentroCosto_Asigna(codEmpresa, param);
+
+        [HttpPost("CntXAcMiembros_Asigna")]
+        public ActionResult<ErrorDto<bool>> CntXAcMiembros_Asigna([FromQuery] int codEmpresa, [FromBody] CntXAcMiembroAsignaParams param)
+            => _bl.CntXAcMiembros_Asigna(codEmpresa, param);
+
+        [HttpPost("CntXAcRol_Add")]
+        public ActionResult<ErrorDto<bool>> CntXAcRol_Add([FromQuery] int codEmpresa, [FromBody] CntXAcRolAddParams param)
+            => _bl.CntXAcRol_Add(codEmpresa, param);
+
+        [HttpPost("CntXAcRol_Delete")]
+        public ActionResult<ErrorDto<bool>> CntXAcRol_Delete([FromQuery] int codEmpresa, [FromBody] CntXAcRolDeleteParams param)
+            => _bl.CntXAcRol_Delete(codEmpresa, param);
     }
 }
