31f03f5e19a35241e6b62859f37dec9d1d0b5baf
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-03-25 17:37:45 -0600

    ajuste Controllers

--- Galileo_API/Controllers/ProGrX/Creditos/frmCR_CatalogoGarantiasController.cs
+++ Galileo_API/Controllers/ProGrX/Creditos/frmCR_CatalogoGarantiasController.cs
@@ -4,7 +4,7 @@ using Galileo_API.Models.ProGrX.Credito;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Galileo_API.Controllers.ProGrX.Credito
+namespace Galileo_API.Controllers.ProGrX.Creditos
 {
     [Route("api/[controller]")]
     [ApiController]
