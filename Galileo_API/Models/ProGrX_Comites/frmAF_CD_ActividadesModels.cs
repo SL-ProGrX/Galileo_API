54fd42f5e21e304bf69f2388664b65b8ca06c92a
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-03-27 17:12:10 -0600

    frmAF_CD_Actividades

---
+++ Galileo_API/Models/ProGrX_Comites/frmAF_CD_ActividadesModels.cs
@@ -0,0 +1,35 @@
+﻿namespace Galileo_API.Models.ProGrX_Comites
+{
+    public class AfCdActividadDto
+    {
+        public int Cod_Actividad { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Cod_Cuenta { get; set; }
+        public DateTime? FechaPeriocidad { get; set; }
+        public DateTime? FechaLiq { get; set; }
+        public bool Activa { get; set; }
+        public string? CuentaX { get; set; }
+        public string? Tipo { get; set; }
+    }
+
+    public class AfCdActividadComiteDto
+    {
+        public int Cod_Actividad { get; set; }
+        public int Cod_Comite { get; set; }
+        public string? Descripcion { get; set; }
+    }
+
+    public class AfCdActividadSimpleDto
+    {
+        public int Cod_Actividad { get; set; }
+        public string? Descripcion { get; set; }
+    }
+
+    public class AfCdActividadRangoDto
+    {
+        public int Cod_Monto { get; set; }
+        public decimal Monto { get; set; }
+        public decimal Minimo { get; set; }
+        public decimal Maximo { get; set; }
+    }
+}
