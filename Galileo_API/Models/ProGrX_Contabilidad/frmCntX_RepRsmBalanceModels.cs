bed0b662b81b5e6787d8defb499207921a0b699d
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-03-27 15:23:53 -0600

    Desarrollo frmCntX_RepRsmBalance

---
+++ Galileo_API/Models/ProGrX_Contabilidad/frmCntX_RepRsmBalanceModels.cs
@@ -0,0 +1,10 @@
+﻿namespace Galileo_API.Models.ProGrX_Contabilidad
+{
+    public class CntxRsmBalanceFiltroDto
+    {
+        public string? unidad { get; set; }
+        public string? centroCosto { get; set; }
+        public string? reporte { get; set; }
+        public string? usuario { get; set; }
+    }
+}
