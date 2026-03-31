935d3f02bbd82839725b9490114f6c8a9a1565ad
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-03-31 17:04:11 -0600

    Desarrollo frmCxC_Reportes

---
+++ Galileo_API/DataBaseTier/ProGrX/CuentasxCobrar/frmCxC_ReportesDB.cs
@@ -0,0 +1,109 @@
+﻿using Galileo.DataBaseTier;
+using Galileo.Models;
+using Galileo.Models.ERROR;
+
+namespace Galileo_API.DataBaseTier.ProGrX_CxC
+{
+    public class FrmCxCReportesDb
+    {
+        private readonly PortalDB _portalDb;
+
+        public FrmCxCReportesDb(IConfiguration config)
+            : this(new PortalDB(config))
+        {
+        }
+
+        public FrmCxCReportesDb(PortalDB portalDb)
+        {
+            _portalDb = portalDb;
+        }
+
+        /// <summary>
+        /// Lista los clientes
+        /// </summary>
+        /// <param name="codEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> CxC_Clientes_Listar(int codEmpresa)
+        {
+            const string sql = @"
+                SELECT 
+                    RTRIM(cedula) AS item,
+                    RTRIM(nombre) AS descripcion
+                FROM CxC_Personas
+                ORDER BY nombre
+            ";
+
+            return DbHelper.ExecuteListQuery<DropDownListaGenericaModel>(
+                _portalDb,
+                codEmpresa,
+                sql
+            );
+        }
+
+        /// <summary>
+        /// Lista los pagadores
+        /// </summary>
+        /// <param name="codEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> CxC_Pagadores_Listar(int codEmpresa)
+        {
+            const string sql = @"
+                SELECT 
+                    RTRIM(cedula) AS item,
+                    RTRIM(nombre) AS descripcion
+                FROM CxC_Personas
+                ORDER BY nombre
+            ";
+
+            return DbHelper.ExecuteListQuery<DropDownListaGenericaModel>(
+                _portalDb,
+                codEmpresa,
+                sql
+            );
+        }
+
+        /// <summary>
+        /// Lista los conceptos
+        /// </summary>
+        /// <param name="codEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> CxC_Conceptos_Listar(int codEmpresa)
+        {
+            const string sql = @"
+                SELECT 
+                    RTRIM(cod_concepto) AS item,
+                    RTRIM(descripcion) AS descripcion
+                FROM CxC_Conceptos
+                ORDER BY descripcion
+            ";
+
+            return DbHelper.ExecuteListQuery<DropDownListaGenericaModel>(
+                _portalDb,
+                codEmpresa,
+                sql
+            );
+        }
+
+        /// <summary>
+        /// Lista los cargos
+        /// </summary>
+        /// <param name="codEmpresa"></param>
+        /// <returns></returns>
+        public ErrorDto<List<DropDownListaGenericaModel>> CxC_Cargos_Listar(int codEmpresa)
+        {
+            const string sql = @"
+                SELECT 
+                    RTRIM(cod_cargo) AS item,
+                    RTRIM(descripcion) AS descripcion
+                FROM CxC_Cargos
+                ORDER BY descripcion
+            ";
+
+            return DbHelper.ExecuteListQuery<DropDownListaGenericaModel>(
+                _portalDb,
+                codEmpresa,
+                sql
+            );
+        }
+    }
+}
