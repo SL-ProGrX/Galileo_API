832d81da610038015f1089a4f90e189d620d7bd0
Marco Espinoza <mespinoza@mpbsystemlogic.com>
2026-04-07 20:50:35 -0600

    frmCO_Notifica_Asociado_Aportes_Atrasados

---
+++ Galileo_API/BusinessLogic/ProGrX/Cobros/frmCO_Notifica_Asociado_Aportes_AtrasadosBL.cs
@@ -0,0 +1,37 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Cobros;
+using Galileo_API.Models.ProGrX.Cobros;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Cobros
+{
+    public class FrmCONotificaAsociadoAportesAtrasadosBL
+    {
+        private readonly FrmCONotificaAsociadoAportesAtrasadosDB Db;
+
+        public FrmCONotificaAsociadoAportesAtrasadosBL(IConfiguration config)
+        {
+            Db = new FrmCONotificaAsociadoAportesAtrasadosDB(config);
+        }
+
+        public ErrorDto<CoNotificaAsociadoAportesAtrasadosListaResult> CO_Notifica_Asociado_Aportes_Atrasados_Lista_Obtener(
+            int CodEmpresa,
+            string? cedula)
+        {
+            return Db.CO_Notifica_Asociado_Aportes_Atrasados_Lista_Obtener(CodEmpresa, cedula);
+        }
+
+        public ErrorDto<CoNotificaAsociadoAportesAtrasadosListaResult> CO_Notifica_Asociado_Aportes_Atrasados_Lista_Export(
+            int CodEmpresa,
+            string? cedula)
+        {
+            return Db.CO_Notifica_Asociado_Aportes_Atrasados_Lista_Export(CodEmpresa, cedula);
+        }
+
+        public ErrorDto CO_Notifica_Asociado_Aportes_Atrasados_Enviar(
+            int CodEmpresa,
+            CoNotificaAsociadoAportesAtrasadosEnviarRequest req)
+        {
+            return Db.CO_Notifica_Asociado_Aportes_Atrasados_Enviar(CodEmpresa, req);
+        }
+    }
+}
