fa14d55950f8685ac4d3d60cc9c569f6f38c01bc
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-04-08 17:06:03 -0600

    frmAF_CD_RemesasComites

---
+++ Galileo_API/Controllers/ProGrX_Comites/frmAF_CD_RemesasComitesController.cs
@@ -0,0 +1,32 @@
+﻿using Galileo_API.BusinessLogic.ProGrX_Comites;
+using Galileo_API.Models.ProGrX_Comites;
+using Galileo.Models.ERROR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Galileo_API.Controllers.ProGrX_Comites
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FrmAfCdRemesasComitesController : ControllerBase
+    {
+        private readonly FrmAfCdRemesasComitesBL _bl;
+
+        public FrmAfCdRemesasComitesController(IConfiguration config)
+        {
+            _bl = new FrmAfCdRemesasComitesBL(config);
+        }
+
+        [HttpGet("AfCdRemesasTes_Lista")]
+        public ActionResult<ErrorDto<List<AfCdRemesaTesDto>>> AfCdRemesasTes_Lista([FromQuery] int codEmpresa)
+            => _bl.AfCdRemesasTes_Lista(codEmpresa);
+
+        [HttpPost("AfCdRemesasTes_Guardar")]
+        public ActionResult<ErrorDto<bool>> AfCdRemesasTes_Guardar([FromQuery] int codEmpresa, [FromBody] AfCdRemesaTesSaveDto dto)
+            => _bl.AfCdRemesasTes_Guardar(codEmpresa, dto);
+
+        [HttpDelete("AfCdRemesasTes_Eliminar")]
+        public ActionResult<ErrorDto<bool>> AfCdRemesasTes_Eliminar([FromQuery] int codEmpresa, [FromQuery] int codRemesa)
+            => _bl.AfCdRemesasTes_Eliminar(codEmpresa, codRemesa);
+    }
+}
---
+++ Galileo_API/Models/ProGrX_Comites/frmAF_CD_RemesasComitesModels.cs
@@ -0,0 +1,22 @@
+﻿namespace Galileo_API.Models.ProGrX_Comites
+{
+    public class AfCdRemesaTesDto
+    {
+        public int Cod_Remesa { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string? Usuario { get; set; }
+        public DateTime? Fecha_Inicio { get; set; }
+        public DateTime? Fecha_Corte { get; set; }
+        public string? Notas { get; set; }
+        public string? Estado { get; set; }
+    }
+
+    public class AfCdRemesaTesSaveDto
+    {
+        public int Cod_Remesa { get; set; } // 0 para insertar, >0 para actualizar
+        public string Usuario { get; set; } = string.Empty;
+        public DateTime? Fecha_Inicio { get; set; }
+        public DateTime? Fecha_Corte { get; set; }
+        public string? Notas { get; set; }
+    }
+}
