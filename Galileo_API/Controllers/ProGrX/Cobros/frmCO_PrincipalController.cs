c3c2966950d13836da1980f6c2713a415e01ef79
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-04-10 18:22:54 -0600

    Desarrollo frmCO_Principal

--- Galileo_API/Controllers/ProGrX/Cobros/frmCO_PrincipalController.cs
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_PrincipalController.cs
@@ -3,6 +3,7 @@ using Galileo_API.Models.ProGrX.Cobros;
 using Galileo_API.BusinessLogic.ProGrX.Cobros;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Galileo.Models;
 
 namespace Galileo_API.Controllers.ProGrX.Cobros
 {
@@ -27,6 +28,13 @@ namespace Galileo_API.Controllers.ProGrX.Cobros
         public ErrorDto<OperacionConsultarDto> Operacion_Consultar(int codEmpresa, int operacion)
         {
             return _bl.Operacion_Consultar(codEmpresa, operacion);
+        }
+
+        [Authorize]
+        [HttpGet("Deductoras_Listar")]
+        public ErrorDto<List<DropDownListaGenericaModel>> Deductoras_Listar(int CodEmpresa, int codInstitucion)
+        {
+            return _bl.Deductoras_Listar(CodEmpresa, codInstitucion);
         }
     }
 }
