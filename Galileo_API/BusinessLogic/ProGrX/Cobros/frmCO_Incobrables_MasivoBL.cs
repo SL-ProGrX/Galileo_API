359c8efd036289cc7a7418f395a0380526d2d353
dizuniga <dzuniga@mpbsystemlogic.com>
2026-04-18 11:19:40 -0600

    frmCO_Incobrables_Masivo

---
+++ Galileo_API/BusinessLogic/ProGrX/Cobros/frmCO_Incobrables_MasivoBL.cs
@@ -0,0 +1,25 @@
+﻿
+using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Cobros;
+using static Galileo_API.Models.ProGrX.Cobros.FrmCoIncobrablesMasivoModels;
+
+
+namespace Galileo_API.BusinessLogic.ProGrX.Cobros
+{
+    public class FrmCoIncobrablesMasivoBL
+    {
+        private readonly FrmCoIncobrablesMasivoDB _db;
+
+        public FrmCoIncobrablesMasivoBL(IConfiguration config)
+        {
+            _db = new FrmCoIncobrablesMasivoDB(config);
+        }
+
+        public ErrorDto<CoIncobrablesMasivoCargaResponse> Co_IncobrablesMasivo_CargarArchivo(int codEmpresa, List<string> operaciones, string usuario)
+                 => _db.Co_IncobrablesMasivo_CargarArchivo(codEmpresa, operaciones, usuario);
+       
+        public ErrorDto<bool> Co_IncobrablesMasivo_Procesar(int codEmpresa, string nota, string usuario)
+              => _db.Co_IncobrablesMasivo_Procesar(codEmpresa, nota, usuario);
+           
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_Incobrables_MasivoController.cs
@@ -0,0 +1,35 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Cobros;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static Galileo_API.Models.ProGrX.Cobros.FrmCoIncobrablesMasivoModels;
+
+
+namespace Galileo_API.Controllers.ProGrX.Cobros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FrmCoIncobrablesMasivoController : ControllerBase
+    {
+        private readonly FrmCoIncobrablesMasivoBL _bl;
+        public FrmCoIncobrablesMasivoController(IConfiguration config)
+   => _bl = new FrmCoIncobrablesMasivoBL(config);
+
+        [Authorize]
+        [HttpPost("Co_IncobrablesMasivo_CargarArchivo")]
+        public ErrorDto<CoIncobrablesMasivoCargaResponse> Co_IncobrablesMasivo_CargarArchivo(
+                    int codEmpresa, [FromBody] CoIncobrablesMasivoCargaRequest request)
+    => _bl.Co_IncobrablesMasivo_CargarArchivo(
+        codEmpresa,
+        request.Operaciones,
+        request.Usuario);
+
+
+        [Authorize]
+        [HttpPost("Co_IncobrablesMasivo_Procesar")]
+        public ErrorDto<bool> Co_IncobrablesMasivo_Procesar(int codEmpresa,  string usuario,string notas )
+              => _bl.Co_IncobrablesMasivo_Procesar(codEmpresa, notas, usuario);
+
+    }
+}
