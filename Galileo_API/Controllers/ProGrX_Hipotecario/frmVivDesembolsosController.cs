24222c8fc8e067bb7a083df46e640ae68b2b11ff
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-04-21 22:12:08 -0600

    Desarrollo frmVivDesembolsos

---
+++ Galileo_API/Controllers/ProGrX_Hipotecario/frmVivDesembolsosController.cs
@@ -0,0 +1,31 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX_Hipotecario;
+using Galileo_API.Models.ProGrX.Cobros;
+using Galileo_API.Models.ProGrX_Hipotecario;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Hipotecario
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmVivDesembolsosController : ControllerBase
+    {
+        private readonly FrmVivDesembolsosBl _bl;
+
+        public FrmVivDesembolsosController(IConfiguration config)
+        {
+            _bl = new FrmVivDesembolsosBl(config);
+        }
+
+
+        [Authorize]
+        [HttpGet("Operaciones_Listar")]
+        public ErrorDto<List<OperacionBusquedaDto>> Operaciones_Listar(int codEmpresa)
+        {
+            return _bl.Operaciones_Listar(codEmpresa);
+        }
+    }
+}
---
+++ Galileo_API/DataBaseTier/ProGrX_Hipotecario/frmVivDesembolsosDB.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using Galileo.DataBaseTier;
+using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.Models.ProGrX.Cobros;
+using Galileo_API.Models.ProGrX_Hipotecario;
+using Microsoft.Data.SqlClient;
+
+namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
+{
+    public class FrmVivDesembolsosDb
+    {
+        private readonly PortalDB _portalDb;
+        private readonly MSecurityMainDb _bitacora;
+        private readonly int vModulo = 3;
+
+        public FrmVivDesembolsosDb(IConfiguration config)
+           : this(
+                 new PortalDB(config),
+                 new MSecurityMainDb(config))
+        {
+        }
+
+        public FrmVivDesembolsosDb(PortalDB portalDB, MSecurityMainDb dbBitacora)
+        {
+            _portalDb = portalDB;
+            _bitacora = dbBitacora;
+        }
+
+        public ErrorDto<List<OperacionBusquedaDto>> Operaciones_Listar(int codEmpresa)
+        {
+            var response = new ErrorDto<List<OperacionBusquedaDto>>();
+
+            try
+            {
+                using var cn = new SqlConnection(
+                    _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+                const string sql = @"
+                                SELECT TOP 10
+                                    id_solicitud AS operacion,
+                                    RTRIM(codigo) AS codigo,
+                                    RTRIM(cedula) AS cedula,
+                                    montoapr,
+                                    saldo
+                                FROM reg_creditos
+                                WHERE estadosol = 'F'
+                                ORDER BY id_solicitud
+                            ";
+
+                response.Result = cn.Query<OperacionBusquedaDto>(sql).ToList();
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+    }
+}
