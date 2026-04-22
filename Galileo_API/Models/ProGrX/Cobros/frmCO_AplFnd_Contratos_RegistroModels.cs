55a6577def8d24306bb78daa48036e3d11e51c54
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-04-22 17:39:08 -0600

    Update frmCO_AplFnd_Contratos_RegistroModels.cs

--- Galileo_API/Models/ProGrX/Cobros/frmCO_AplFnd_Contratos_RegistroModels.cs
+++ Galileo_API/Models/ProGrX/Cobros/frmCO_AplFnd_Contratos_RegistroModels.cs
@@ -50,12 +50,12 @@
 
     public class CoAplFndContratosRegistroGuardarRequest
     {
-        public long id_contrato { get; set; }
+        public long id_contrato { get; set; } = 0;
         public string cedula { get; set; } = string.Empty;
         public DateTime? firma_contrato { get; set; }
         public DateTime? fecha_vencimiento { get; set; }
-        public int estado { get; set; }
-        public long id_solicitud { get; set; }
+        public int estado { get; set; } = 0;
+        public long id_solicitud { get; set; } = 0;
         public string observaciones { get; set; } = string.Empty;
         public string usuario { get; set; } = string.Empty;
     }
