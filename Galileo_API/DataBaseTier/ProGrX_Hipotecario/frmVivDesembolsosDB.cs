f017d30e2d7671b4058cc8dee8a6d612818d541e
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-04-21 22:20:00 -0600

    update

--- Galileo_API/DataBaseTier/ProGrX_Hipotecario/frmVivDesembolsosDB.cs
+++ Galileo_API/DataBaseTier/ProGrX_Hipotecario/frmVivDesembolsosDB.cs
@@ -11,7 +11,6 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
     public class FrmVivDesembolsosDb
     {
         private readonly PortalDB _portalDb;
-        private readonly MSecurityMainDb _bitacora;
         private readonly int vModulo = 3;
 
         public FrmVivDesembolsosDb(IConfiguration config)
@@ -24,7 +23,7 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
         public FrmVivDesembolsosDb(PortalDB portalDB, MSecurityMainDb dbBitacora)
         {
             _portalDb = portalDB;
-            _bitacora = dbBitacora;
+           
         }
 
         public ErrorDto<List<OperacionBusquedaDto>> Operaciones_Listar(int codEmpresa)
