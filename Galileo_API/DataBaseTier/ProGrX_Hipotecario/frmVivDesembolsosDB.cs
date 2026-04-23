1e5af3f5bd711e385336fd6e4bceeedf600bd146
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-04-23 17:50:11 -0600

    Desarrollo frmVivDesembolsos

--- Galileo_API/DataBaseTier/ProGrX_Hipotecario/frmVivDesembolsosDB.cs
+++ Galileo_API/DataBaseTier/ProGrX_Hipotecario/frmVivDesembolsosDB.cs
@@ -6,6 +6,7 @@ using Galileo_API.Models.ProGrX.Cobros;
 using Galileo_API.Models.ProGrX_Hipotecario;
 using Microsoft.Data.SqlClient;
 using System;
+using System.Data;
 
 namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
 {
@@ -98,6 +99,7 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
             {
                 const string sql = @"
                     SELECT
+                        CodigoDesembolso AS codigo,
                         RTRIM(Beneficiario) AS beneficiario,
                         ISNULL(Monto, 0) AS monto,
                         ISNULL(disponible, 0) AS disponible,
@@ -210,7 +212,7 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
                 return cn.Query<ConceptoApiDto>(sql).ToList();
             });
         }
-        public ErrorDto<bool> PermiteDesembolso(int codEmpresa,int operacion,int index)
+        public ErrorDto<bool> PermiteDesembolso(int codEmpresa, int operacion, int index)
         {
             var response = new ErrorDto<bool>();
 
@@ -221,7 +223,7 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
 
                 string sql;
 
-                if (index == 2) 
+                if (index == 2)
                 {
                     sql = @"
                 SELECT COUNT(*) 
@@ -230,7 +232,7 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
                 AND ID_SOLICITUD = @Operacion
             ";
                 }
-                else 
+                else
                 {
                     sql = @"
                 SELECT COUNT(*)
@@ -253,6 +255,262 @@ namespace Galileo_API.DataBaseTier.ProGrX_Hipotecario
             }
             catch (Exception ex)
             {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+
+        public ErrorDto<bool> ActivarDesembolsoPendiente(int codEmpresa, ActivarDesembolsoPendienteRequestDto request)
+        {
+            var response = new ErrorDto<bool>();
+
+            try
+            {
+                using var cn = new SqlConnection(
+                    _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+                var p = new DynamicParameters();
+
+                p.Add("@Contacto", request.contacto);
+                p.Add("@Tipo", request.tipo);
+                p.Add("@Garantia", request.garantia);
+                p.Add("@LineaDesembolso", request.linea);
+                p.Add("@Usuario", request.usuario);
+
+                p.Add("@Descuento", request.descuento, DbType.Decimal, precision: 12, scale: 2);
+                p.Add("@Disponible", request.disponible, DbType.Decimal, precision: 13, scale: 2);
+
+                p.Add("@FechaCorte", request.fechaCorte);
+                p.Add("@FechaUltimoCorte", request.fechaUltimoCorte);
+
+                p.Add("@InteresesActualDias", request.diasActuales);
+                p.Add("@InteresesActualMonto", request.interesesActuales, DbType.Decimal, precision: 12, scale: 2);
+
+                p.Add("@InteresesAcumDias", request.diasAcumulados);
+                p.Add("@InteresesAcumMonto", request.interesesAcumulados, DbType.Decimal, precision: 12, scale: 2);
+
+                cn.Execute(
+                    "dbo.spCRDViviendaDesembolsoPendienteActiva",
+                    p,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+
+        public ErrorDto<CambioPendienteResponseDto> Pendiente_Cambiar(int codEmpresa, CambioPendienteRequestDto request)
+        {
+            var response = new ErrorDto<CambioPendienteResponseDto>();
+
+            try
+            {
+                using var cn = new SqlConnection(
+                    _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+                var sql = @"
+            exec spVivDesembolso_Pendiente_Cambios 
+                @Garantia,
+                @Tipo,
+                @Linea,
+                @Contacto,
+                @Usuario";
+
+                var result = cn.QueryFirst<CambioPendienteResponseDto>(
+                    sql,
+                    new
+                    {
+                        Garantia = request.garantia,
+                        Tipo = request.tipo,
+                        Linea = request.linea,
+                        Contacto = request.contacto,
+                        Usuario = request.usuario
+                    });
+
+                response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+
+        public ErrorDto<bool> Pendiente_Agregar(int codEmpresa, AgregarPendienteRequestDto request)
+        {
+            var response = new ErrorDto<bool>();
+
+            try
+            {
+                using var cn = new SqlConnection(
+                    _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+                var p = new DynamicParameters();
+
+                p.Add("@Contacto", request.contacto);
+                p.Add("@IdGarantia", request.garantia);
+                p.Add("@TipoDesembolso", request.tipoDesembolso);
+                p.Add("@Beneficiario", request.beneficiario);
+
+                p.Add("@Monto", request.monto, DbType.Decimal, precision: 12, scale: 2);
+
+                p.Add("@AplicaIntereses", request.aplicaIntereses);
+                p.Add("@Usuario", request.usuario);
+
+                p.Add("@Fecha", request.fecha);
+
+                p.Add("@Disponible", request.disponible, DbType.Decimal, precision: 14, scale: 2);
+
+                cn.Execute(
+                    "dbo.spCRDVivDesembolsosPendientes_A",
+                    p,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+
+        public ErrorDto<bool> DesembolsoDetalle_Guardar(int codEmpresa, List<DesembolsoDetalleDto> detalles)
+        {
+            var response = new ErrorDto<bool>();
+
+            try
+            {
+                using var cn = new SqlConnection(
+                    _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+                using var tx = cn.BeginTransaction();
+
+                foreach (var item in detalles)
+                {
+                    var p = new DynamicParameters();
+
+                    p.Add("@CodigoDesembolso", item.codigodesembolso);
+                    p.Add("@Codigo", item.codigo);
+                    p.Add("@Monto", item.monto, DbType.Decimal, precision: 12, scale: 2);
+                    p.Add("@RegistroUsuario", item.usuario);
+
+                    cn.Execute(
+                        "dbo.spCRDVivDesembolsoDetalle_A",
+                        p,
+                        transaction: tx,
+                        commandType: CommandType.StoredProcedure
+                    );
+                }
+
+                tx.Commit();
+                response.Result = true;
+            }
+            catch (Exception ex)
+            {
+                response.Code = -1;
+                response.Description = ex.Message;
+            }
+
+            return response;
+        }
+
+
+        public ErrorDto<int> Desembolso_Guardar(int codEmpresa,ViviendaDesembolsoRequestDto request)
+        {
+            var response = new ErrorDto<int>();
+
+            using var cn = new SqlConnection(
+                _portalDb.ObtenerDbConnStringEmpresa(codEmpresa));
+
+            cn.Open();
+            using var tx = cn.BeginTransaction();
+
+            try
+            {
+
+                var p = new DynamicParameters();
+
+                p.Add("@CodigoDesembolso", request.codigodesembolso, DbType.Int32, ParameterDirection.InputOutput);
+                p.Add("@NumeroOperacion", request.numeroOperacion);
+                p.Add("@RegistroUsuario", request.registroUsuario);
+                p.Add("@Beneficiario", request.beneficiario);
+
+                p.Add("@Monto", request.monto, DbType.Decimal, precision: 13, scale: 2);
+                p.Add("@Detalle", request.detalle);
+                p.Add("@Disponible", request.disponible, DbType.Decimal, precision: 13, scale: 2);
+
+                p.Add("@AplicaIntereses", request.aplicaIntereses);
+                p.Add("@FechaCorte", request.fechaCorte);
+                p.Add("@FechaUltimoCorte", request.fechaUltimoCorte);
+
+                p.Add("@InteresesActualDias", request.interesesActualDias);
+                p.Add("@InteresesActualMonto", request.interesesActualMonto, DbType.Decimal, precision: 12, scale: 2);
+
+                p.Add("@InteresesAcumDias", request.interesesAcumDias);
+                p.Add("@InteresesAcumMonto", request.interesesAcumMonto, DbType.Decimal, precision: 12, scale: 2);
+
+                p.Add("@BancoCodigo", request.bancoCodigo);
+                p.Add("@BancoEmitir", request.bancoEmitir);
+                p.Add("@BancoCuenta", request.bancoCuenta);
+                p.Add("@Identificacion", request.identificacion);
+
+                var id = cn.QueryFirstOrDefault<int>(
+                    "dbo.spCRDVivDesembolsos_A",
+                    p,
+                    transaction: tx,
+                    commandType: CommandType.StoredProcedure
+                );
+
+                int idDesembolso = id > 0
+                    ? id
+                    : p.Get<int>("@CodigoDesembolso");
+
+                if (idDesembolso <= 0)
+                    throw new Exception("No se pudo generar el desembolso");
+
+                foreach (var item in request.detalles)
+                {
+                    var pd = new DynamicParameters();
+
+                    pd.Add("@CodigoDesembolso", idDesembolso);
+                    pd.Add("@Codigo", item.codigo);
+                    pd.Add("@Monto", item.monto, DbType.Decimal, precision: 12, scale: 2);
+                    pd.Add("@RegistroUsuario", request.registroUsuario);
+
+                    cn.Execute(
+                        "dbo.spCRDVivDesembolsoDetalle_A",
+                        pd,
+                        transaction: tx,
+                        commandType: CommandType.StoredProcedure
+                    );
+                }
+                tx.Commit();
+
+                response.Result = idDesembolso;
+            }
+            catch (Exception ex)
+            {
+                tx.Rollback();
+
                 response.Code = -1;
                 response.Description = ex.Message;
             }
