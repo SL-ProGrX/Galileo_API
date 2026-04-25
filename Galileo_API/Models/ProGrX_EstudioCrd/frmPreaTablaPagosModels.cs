50e3b4b72eb49fbe64352d5fbc97788546b5e13b
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-04-24 18:52:50 -0600

    Desarrollo de frmPreaTablaPagos

---
+++ Galileo_API/Models/ProGrX_EstudioCrd/frmPreaTablaPagosModels.cs
@@ -0,0 +1,13 @@
+﻿namespace Galileo_API.Models.ProGrX_EstudioCrd
+{
+    public class CrdPreaTablaPagosData
+    {
+        public int idx { get; set; }
+        public DateTime? fecha { get; set; }
+        public string usuario { get; set; } = string.Empty;
+        public DateTime? inicio { get; set; }
+        public DateTime? corte { get; set; }
+        public int npagos { get; set; } = 0;
+        public int cod_institucion { get; set; } = 0;
+    }
+}
