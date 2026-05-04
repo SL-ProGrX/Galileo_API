d2d6ff63955e5e0835cc9f5080fbb706ffe35393
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-05-04 15:20:11 -0600

    frmCO_ApFndprioridades

---
+++ Galileo_API/BusinessLogic/ProGrX/Cobros/frmCO_AplFnd_PrioridadesBL.cs
@@ -0,0 +1,52 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Cobros;
+using static Galileo_API.Models.ProGrX.Cobros.FrmCoAplFndPrioridadesModels;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Cobros
+{
+    public class FrmCoAplFndPrioridadesBl
+    {
+        private readonly FrmCoAplFndPrioridadesDb _db;
+
+        public FrmCoAplFndPrioridadesBl(IConfiguration config)
+        {
+            _db = new FrmCoAplFndPrioridadesDb(config);
+        }
+
+        public ErrorDto<COAplFndPrioridadesListaResult> Co_AplFnd_Prioridades_Lista_Obtener(int CodEmpresa, string jfiltros)
+        {
+            return _db.Co_AplFnd_Prioridades_Lista_Obtener(CodEmpresa, jfiltros);
+        }
+
+        public ErrorDto<COAplFndPrioridadesListaResult> Co_AplFnd_Prioridades_Lista_Export(int CodEmpresa, string jfiltros)
+        {
+            return _db.Co_AplFnd_Prioridades_Lista_Export(CodEmpresa, jfiltros);
+        }
+
+        public ErrorDto Co_AplFnd_Prioridades_Guardar(int CodEmpresa, string usuario, COAplFndPrioridadData prioridad)
+        {
+            return _db.Co_AplFnd_Prioridades_Guardar(CodEmpresa, usuario, prioridad);
+        }
+
+        public ErrorDto Co_AplFnd_Prioridades_Eliminar(int CodEmpresa, string usuario, string codigo)
+        {
+            return _db.Co_AplFnd_Prioridades_Eliminar(CodEmpresa, usuario, codigo);
+        }
+
+        public ErrorDto<List<DropDownListaGenericaModel>> Co_AplFnd_Prioridades_GarantiasDisponibles_Obtener(int CodEmpresa)
+        {
+            return _db.Co_AplFnd_Prioridades_GarantiasDisponibles_Obtener(CodEmpresa);
+        }
+
+        public ErrorDto<int> Co_AplFnd_PrioridadEjecucion_Obtener(int CodEmpresa)
+        {
+            return _db.Co_AplFnd_PrioridadEjecucion_Obtener(CodEmpresa);
+        }
+
+        public ErrorDto Co_AplFnd_PrioridadEjecucion_Actualizar(int CodEmpresa, string usuario, int prioridad)
+        {
+            return _db.Co_AplFnd_PrioridadEjecucion_Actualizar(CodEmpresa, usuario, prioridad);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Cobros/frmCO_AplFnd_PrioridadesModels.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Galileo_API.Models.ProGrX.Cobros
+{
+    public class FrmCoAplFndPrioridadesModels
+    {
+        public class COAplFndPrioridadesListaResult
+        {
+            public int total { get; set; }
+            public List<COAplFndPrioridadData> lista { get; set; } = new();
+        }
+
+        public class COAplFndPrioridadData
+        {
+            public string codigo { get; set; } = string.Empty;
+            public string descripcion { get; set; } = string.Empty;
+            public int orden { get; set; } = 0;
+            public bool activo { get; set; } = false;
+            public string registro_fecha { get; set; } = string.Empty;
+            public string registro_usuario { get; set; } = string.Empty;
+            public string modifica_fecha { get; set; } = string.Empty;
+            public string modifica_usuario { get; set; } = string.Empty;
+
+            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+            public bool isNew { get; set; }
+        }
+
+        public static class CoAplFndPrioridadConst
+        {
+            public const int vModulo = 4;
+
+            public const string SF_CODIGO = "codigo";
+            public const string SF_DESCRIPCION = "descripcion";
+            public const string SF_ORDEN = "orden";
+            public const string SF_ACTIVO = "activo";
+            public const string SF_REGISTRO_FECHA = "registro_fecha";
+            public const string SF_REGISTRO_USUARIO = "registro_usuario";
+            public const string SF_MODIFICA_FECHA = "modifica_fecha";
+            public const string SF_MODIFICA_USUARIO = "modifica_usuario";
+
+            public const string SP_LISTA = @"EXEC spCBR_Pagos_Apl_Config_Prioridades_Lista;";
+            public const string SP_ADD = @"EXEC spCBR_Pagos_Apl_Config_Prioridades_Add @Codigo, @Orden, @Activo, @Usuario;";
+            public const string SP_UPD = @"EXEC spCBR_Pagos_Apl_Config_Prioridades_Add @Codigo, @Orden, @Activo, @Usuario;";
+            public const string SP_DEL = @"EXEC spCBR_Pagos_Apl_Config_Prioridades_Del @Codigo, @Usuario;";
+            public const string SP_PRIORIDAD_EJECUCION = @"EXEC spCBR_Pagos_Apl_Config_Prioridad_Ejecucion_Actualiza @Valor, @Usuario;";
+        }
+    }
+}
