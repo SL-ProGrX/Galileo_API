e620fe11db11c7012c54844532893cf5bdfca844
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-05-08 13:32:31 -0600

    Desarrollo frmPreaTablaImpRenta

---
+++ Galileo_API/Models/ProGrX_EstudioCrd/frmPreaTablaImpRentaModels.cs
@@ -0,0 +1,11 @@
+namespace Galileo_API.Models.ProGrX_EstudioCrd
+{
+    public class CrdPreaTablaImpRentaData
+    {
+        public int idx { get; set; } = 0;
+        public decimal desde { get; set; } = 0;
+        public decimal hasta { get; set; } = 0;
+        public decimal porcentaje { get; set; } = 0;
+        public bool isNew { get; set; } = false;
+    }
+}
