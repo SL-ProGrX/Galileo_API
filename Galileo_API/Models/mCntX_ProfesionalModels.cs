9e6c2af65a5c488b54c1c102abb9635cab0077a7
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-05-08 18:03:46 -0600

    Update mCntX_ProfesionalModels.cs

--- Galileo_API/Models/mCntX_ProfesionalModels.cs
+++ Galileo_API/Models/mCntX_ProfesionalModels.cs
@@ -2,10 +2,10 @@
 {
     public sealed class ContaMascaraData
     {
-        public int nivel1 { get; set; }
-        public int nivel2 { get; set; }
-        public int nivel3 { get; set; }
-        public int nivel4 { get; set; }
-        public int nivel5 { get; set; }
+        public int nivel1 { get; set; } = 0;
+        public int nivel2 { get; set; } = 0;
+        public int nivel3 { get; set; } = 0;
+        public int nivel4 { get; set; } = 0;
+        public int nivel5 { get; set; } = 0;
     }
 }
