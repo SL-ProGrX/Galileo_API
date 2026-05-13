b25778b639c983d647597f824a5b3813490452ab
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-05-13 15:56:08 -0600

    frmPreaConsultaExpedientes

---
+++ Galileo_API/Controllers/ProGrX_EstudioCrd/frmPreaConsultaExpeditentesController.cs
@@ -0,0 +1,29 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX_EstudioCrd;
+using Galileo_API.Models.ProGrX_EstudioCrd;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX_EstudioCrd
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmPreaConsultaExpeditentesController : Controller
+    {
+        private readonly FrmPreaConsultaExpeditentesBL _bl;
+
+        public FrmPreaConsultaExpeditentesController(IConfiguration configuration)
+        {
+            _bl = new FrmPreaConsultaExpeditentesBL(configuration);
+        }
+
+        [HttpGet("Prea_frmPreaConsultaExpeditentes_Lista_Obtener")]
+        public ErrorDto<FrmPreaConsultaExpeditentesListaResponse> Prea_frmPreaConsultaExpeditentes_Lista_Obtener(
+            int codEmpresa,
+            string? buscar = null)
+        {
+            return _bl.Prea_frmPreaConsultaExpeditentes_Lista_Obtener(codEmpresa, buscar);
+        }
+    }
+}
