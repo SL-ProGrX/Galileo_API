29e6a2b098a9a3d353ca9be00f2393ef8068b769
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-05-19 19:42:21 -0600

    frmAF_Motivos_Ingreso

---
+++ Galileo_API/BusinessLogic/ProGrX_Personas/frmAF_Motivos_IngresoBL.cs
@@ -0,0 +1,37 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.Models.ProGrX_Personas;
+using Galileo_API.DataBaseTier.ProGrX_Personas;
+using Newtonsoft.Json;
+
+namespace Galileo_API.BusinessLogic.ProGrX_Personas
+{
+    public class FrmAfMotivosIngresoBL
+    {
+        private readonly FrmAfMotivosIngresoDB _db;
+
+        public FrmAfMotivosIngresoBL(IConfiguration config)
+            => _db = new FrmAfMotivosIngresoDB(config);
+
+        public ErrorDto<MotivoIngresoLista> AF_MotivosIngreso_Obtener(int CodEmpresa, string jfiltros)
+        {
+            FiltrosLazyLoadData? filtros = JsonConvert.DeserializeObject<FiltrosLazyLoadData>(jfiltros);
+            return _db.AF_MotivosIngreso_Obtener(CodEmpresa, filtros);
+        }
+
+        public ErrorDto AF_MotivosIngreso_Valida(int CodEmpresa, string CodMotivo)
+        {
+            return _db.AF_MotivosIngreso_Valida(CodEmpresa, CodMotivo);
+        }
+
+        public ErrorDto AF_MotivosIngreso_Guardar(int CodEmpresa, string Usuario, MotivoIngresoData motivoIngreso)
+        {
+            return _db.AF_MotivosIngreso_Guardar(CodEmpresa, Usuario, motivoIngreso);
+        }
+
+        public ErrorDto AF_MotivosIngreso_Eliminar(int CodEmpresa, string Usuario, string CodMotivo)
+        {
+            return _db.AF_MotivosIngreso_Eliminar(CodEmpresa, Usuario, CodMotivo);
+        }
+    }
+}
--- Galileo_API/Models/ProGrX_Personas/frmAF_Motivos_IngresoModels.cs
+++ Galileo_API/Models/ProGrX_Personas/frmAF_Motivos_IngresoModels.cs
@@ -1,17 +1,17 @@
-﻿namespace Galileo.Models.ProGrX_Personas
+﻿namespace Galileo_API.Models.ProGrX_Personas
 {
     public class MotivoIngresoData
     {
         public string Cod_Motivo { get; set; } = string.Empty;
         public string Descripcion { get; set; } = string.Empty;
         public bool Activo { get; set; } = false;
-        public DateTime Registro_Fecha { get; set; }
+        public DateTime? Registro_Fecha { get; set; }
         public string Registro_Usuario { get; set; } = string.Empty;
     }
 
     public class MotivoIngresoLista
     {
         public int Total { get; set; }
-        public List<MotivoIngresoData> Lista { get; set; } = new List<MotivoIngresoData>();
+        public List<MotivoIngresoData> Lista { get; set; } = [];
     }
 }
