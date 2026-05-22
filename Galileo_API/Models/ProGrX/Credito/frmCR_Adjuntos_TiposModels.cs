b4d9f402bbb602e42a4a6153bde1ea3bf501f3ef
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-05-22 16:05:11 -0600

    Desarrollo de frmCR_Adjuntos_Tipos

---
+++ Galileo_API/Models/ProGrX/Credito/frmCR_Adjuntos_TiposModels.cs
@@ -0,0 +1,23 @@
+﻿namespace Galileo_API.Models.ProGrX.Credito
+{
+    public class CrAdjuntoTipoData
+    {
+        public string cod_adjunto { get; set; } = string.Empty;
+        public string descripcion { get; set; } = string.Empty;
+        public bool activo { get; set; } = false;
+        public string registro_usuario { get; set; } = string.Empty;
+        public DateTime? registro_fecha { get; set; }
+    }
+
+    public class CrAdjuntoTipoGuardarRequest
+    {
+        public string usuario { get; set; } = string.Empty;
+        public CrAdjuntoTipoData tipo { get; set; } = new();
+    }
+
+    public class CrAdjuntoTipoEliminarRequest
+    {
+        public string usuario { get; set; } = string.Empty;
+        public string cod_adjunto { get; set; } = string.Empty;
+    }
+}
