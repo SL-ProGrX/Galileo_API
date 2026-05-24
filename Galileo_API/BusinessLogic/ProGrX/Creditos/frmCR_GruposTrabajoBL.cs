e8a86c6e2f540dce0142547f354f7a51780736db
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-05-24 11:45:51 -0600

    frmCR_GruposTrabajo

---
+++ Galileo_API/BusinessLogic/ProGrX/Creditos/frmCR_GruposTrabajoBL.cs
@@ -0,0 +1,43 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Creditos;
+using Galileo_API.Models.ProGrX.Credito;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Creditos
+{
+    public class FrmCrGruposTrabajoBL
+    {
+        private readonly FrmCrGruposTrabajoDB _db;
+
+        public FrmCrGruposTrabajoBL(IConfiguration config)
+        {
+            _db = new FrmCrGruposTrabajoDB(config);
+        }
+
+        public ErrorDto<List<CrGrupoTrabajoGrupoData>> CR_GruposTrabajo_Grupos_Obtener(int codEmpresa)
+            => _db.CR_GruposTrabajo_Grupos_Obtener(codEmpresa);
+
+        public ErrorDto CR_GruposTrabajo_Grupos_Guardar(int codEmpresa, CrGrupoTrabajoGrupoGuardarRequest request)
+            => _db.CR_GruposTrabajo_Grupos_Guardar(codEmpresa, request);
+
+        public ErrorDto<List<CrGrupoTrabajoGrupoComboData>> CR_GruposTrabajo_GruposCombo_Obtener(int codEmpresa)
+            => _db.CR_GruposTrabajo_GruposCombo_Obtener(codEmpresa);
+
+        public ErrorDto<List<CrGrupoTrabajoMiembroData>> CR_GruposTrabajo_Miembros_Obtener(int codEmpresa, string codGrupo)
+            => _db.CR_GruposTrabajo_Miembros_Obtener(codEmpresa, codGrupo);
+
+        public ErrorDto CR_GruposTrabajo_Miembros_Marcar(int codEmpresa, CrGrupoTrabajoMiembroMarcarRequest request)
+            => _db.CR_GruposTrabajo_Miembros_Marcar(codEmpresa, request);
+
+        public ErrorDto<List<CrGrupoTrabajoEtiquetaData>> CR_GruposTrabajo_Etiquetas_Obtener(int codEmpresa, string codGrupo)
+            => _db.CR_GruposTrabajo_Etiquetas_Obtener(codEmpresa, codGrupo);
+
+        public ErrorDto CR_GruposTrabajo_Etiquetas_Marcar(int codEmpresa, CrGrupoTrabajoEtiquetaMarcarRequest request)
+            => _db.CR_GruposTrabajo_Etiquetas_Marcar(codEmpresa, request);
+
+        public ErrorDto<List<CrGrupoTrabajoComiteData>> CR_GruposTrabajo_Comites_Obtener(int codEmpresa, string codGrupo)
+            => _db.CR_GruposTrabajo_Comites_Obtener(codEmpresa, codGrupo);
+
+        public ErrorDto CR_GruposTrabajo_Comites_Marcar(int codEmpresa, CrGrupoTrabajoComiteMarcarRequest request)
+            => _db.CR_GruposTrabajo_Comites_Marcar(codEmpresa, request);
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX/Creditos/frmCR_GruposTrabajoController.cs
@@ -0,0 +1,57 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Creditos;
+using Galileo_API.Models.ProGrX.Credito;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Creditos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCrGruposTrabajoController : ControllerBase
+    {
+        private readonly FrmCrGruposTrabajoBL _bl;
+
+        public FrmCrGruposTrabajoController(IConfiguration config)
+        {
+            _bl = new FrmCrGruposTrabajoBL(config);
+        }
+
+        [HttpGet("CR_GruposTrabajo_Grupos_Obtener")]
+        public ErrorDto<List<CrGrupoTrabajoGrupoData>> CR_GruposTrabajo_Grupos_Obtener(int codEmpresa)
+            => _bl.CR_GruposTrabajo_Grupos_Obtener(codEmpresa);
+
+        [HttpPost("CR_GruposTrabajo_Grupos_Guardar")]
+        public ErrorDto CR_GruposTrabajo_Grupos_Guardar(int codEmpresa, CrGrupoTrabajoGrupoGuardarRequest request)
+            => _bl.CR_GruposTrabajo_Grupos_Guardar(codEmpresa, request);
+
+        [HttpGet("CR_GruposTrabajo_GruposCombo_Obtener")]
+        public ErrorDto<List<CrGrupoTrabajoGrupoComboData>> CR_GruposTrabajo_GruposCombo_Obtener(int codEmpresa)
+            => _bl.CR_GruposTrabajo_GruposCombo_Obtener(codEmpresa);
+
+        [HttpGet("CR_GruposTrabajo_Miembros_Obtener")]
+        public ErrorDto<List<CrGrupoTrabajoMiembroData>> CR_GruposTrabajo_Miembros_Obtener(int codEmpresa, string codGrupo)
+            => _bl.CR_GruposTrabajo_Miembros_Obtener(codEmpresa, codGrupo);
+
+        [HttpPost("CR_GruposTrabajo_Miembros_Marcar")]
+        public ErrorDto CR_GruposTrabajo_Miembros_Marcar(int codEmpresa, CrGrupoTrabajoMiembroMarcarRequest request)
+            => _bl.CR_GruposTrabajo_Miembros_Marcar(codEmpresa, request);
+
+        [HttpGet("CR_GruposTrabajo_Etiquetas_Obtener")]
+        public ErrorDto<List<CrGrupoTrabajoEtiquetaData>> CR_GruposTrabajo_Etiquetas_Obtener(int codEmpresa, string codGrupo)
+            => _bl.CR_GruposTrabajo_Etiquetas_Obtener(codEmpresa, codGrupo);
+
+        [HttpPost("CR_GruposTrabajo_Etiquetas_Marcar")]
+        public ErrorDto CR_GruposTrabajo_Etiquetas_Marcar(int codEmpresa, CrGrupoTrabajoEtiquetaMarcarRequest request)
+            => _bl.CR_GruposTrabajo_Etiquetas_Marcar(codEmpresa, request);
+
+        [HttpGet("CR_GruposTrabajo_Comites_Obtener")]
+        public ErrorDto<List<CrGrupoTrabajoComiteData>> CR_GruposTrabajo_Comites_Obtener(int codEmpresa, string codGrupo)
+            => _bl.CR_GruposTrabajo_Comites_Obtener(codEmpresa, codGrupo);
+
+        [HttpPost("CR_GruposTrabajo_Comites_Marcar")]
+        public ErrorDto CR_GruposTrabajo_Comites_Marcar(int codEmpresa, CrGrupoTrabajoComiteMarcarRequest request)
+            => _bl.CR_GruposTrabajo_Comites_Marcar(codEmpresa, request);
+    }
+}
