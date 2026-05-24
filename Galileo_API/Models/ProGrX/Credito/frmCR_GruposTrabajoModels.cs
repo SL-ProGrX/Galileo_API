926a00b9f795c06440723b69641988738cc05a9f
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-05-24 11:50:48 -0600

    Update frmCR_GruposTrabajoModels.cs

--- Galileo_API/Models/ProGrX/Credito/frmCR_GruposTrabajoModels.cs
+++ Galileo_API/Models/ProGrX/Credito/frmCR_GruposTrabajoModels.cs
@@ -23,7 +23,7 @@
     {
         public string nombre { get; set; } = string.Empty;
         public string descripcion { get; set; } = string.Empty;
-        public bool asignado { get; set; }
+        public bool asignado { get; set; } = false;
     }
 
     public class CrGrupoTrabajoMiembroMarcarRequest
@@ -31,14 +31,14 @@
         public string usuario_sesion { get; set; } = string.Empty;
         public string cod_grupo { get; set; } = string.Empty;
         public string usuario { get; set; } = string.Empty;
-        public bool marcado { get; set; }
+        public bool marcado { get; set; } = false;
     }
 
     public class CrGrupoTrabajoEtiquetaData
     {
         public string tag_codigo { get; set; } = string.Empty;
         public string descripcion { get; set; } = string.Empty;
-        public bool asignado { get; set; }
+        public bool asignado { get; set; } = false;
     }
 
     public class CrGrupoTrabajoEtiquetaMarcarRequest
@@ -46,21 +46,21 @@
         public string usuario_sesion { get; set; } = string.Empty;
         public string cod_grupo { get; set; } = string.Empty;
         public string tag_codigo { get; set; } = string.Empty;
-        public bool marcado { get; set; }
+        public bool marcado { get; set; } = false;
     }
 
     public class CrGrupoTrabajoComiteData
     {
-        public int id_comite { get; set; }
+        public int id_comite { get; set; } = 0;
         public string descripcion { get; set; } = string.Empty;
-        public bool asignado { get; set; }
+        public bool asignado { get; set; } = false;
     }
 
     public class CrGrupoTrabajoComiteMarcarRequest
     {
         public string usuario_sesion { get; set; } = string.Empty;
         public string cod_grupo { get; set; } = string.Empty;
-        public int id_comite { get; set; }
-        public bool marcado { get; set; }
+        public int id_comite { get; set; } = 0;
+        public bool marcado { get; set; } = false;
     }
 }
