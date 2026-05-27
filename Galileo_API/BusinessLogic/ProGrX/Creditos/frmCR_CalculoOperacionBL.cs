d22238bf9d9b1016e06c9c7975727cf95aa01a73
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-05-26 21:19:44 -0600

    Desarrollo de frmCR_CalculoOperacion

---
+++ Galileo_API/BusinessLogic/ProGrX/Creditos/frmCR_CalculoOperacionBL.cs
@@ -0,0 +1,32 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.Creditos;
+using Galileo_API.Models.ProGrX.Credito;
+using Galileo.Models;
+
+namespace Galileo_API.BusinessLogic.ProGrX.Creditos
+{
+    public class FrmCrCalculoOperacionBl
+    {
+        private readonly FrmCrCalculoOperacionDb _db;
+
+        public FrmCrCalculoOperacionBl(IConfiguration config)
+        {
+            _db = new FrmCrCalculoOperacionDb(config);
+        }
+
+        public ErrorDto<CrCalculoOperacionPantallaData> CrCalculoOperacion_Cedula_Obtener(int codEmpresa, string cedula)
+            => _db.CrCalculoOperacion_Cedula_Obtener(codEmpresa, cedula);
+
+        public ErrorDto<CrCalculoOperacionCodigoData> CrCalculoOperacion_Codigo_Obtener(int codEmpresa, string cedula, string codigo)
+            => _db.CrCalculoOperacion_Codigo_Obtener(codEmpresa, cedula, codigo);
+
+        public ErrorDto<CrCalculoOperacionRangosData> CrCalculoOperacion_Rangos_Obtener(int codEmpresa, string codigo, decimal monto)
+            => _db.CrCalculoOperacion_Rangos_Obtener(codEmpresa, codigo, monto);
+
+        public ErrorDto<List<CrCalculoOperacionDisponibleData>> CrCalculoOperacion_Disponibles_Obtener(int codEmpresa, string cedula)
+            => _db.CrCalculoOperacion_Disponibles_Obtener(codEmpresa, cedula);
+
+        public ErrorDto<List<DropDownListaGenericaModel>> CrCalculoOperacion_Catalogo_Obtener(int codEmpresa)
+            => _db.CrCalculoOperacion_Catalogo_Obtener(codEmpresa);
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX/Creditos/frmCR_CalculoOperacionController.cs
@@ -0,0 +1,43 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Creditos;
+using Galileo_API.Models.ProGrX.Credito;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Galileo.Models;
+
+namespace Galileo_API.Controllers.ProGrX.Creditos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCrCalculoOperacionController : ControllerBase
+    {
+        private readonly FrmCrCalculoOperacionBl _bl;
+
+        public FrmCrCalculoOperacionController(IConfiguration config)
+        {
+            _bl = new FrmCrCalculoOperacionBl(config);
+        }
+
+        [HttpGet("CrCalculoOperacion_Cedula_Obtener")]
+        public ErrorDto<CrCalculoOperacionPantallaData> CrCalculoOperacion_Cedula_Obtener(int codEmpresa, string cedula)
+            => _bl.CrCalculoOperacion_Cedula_Obtener(codEmpresa, cedula);
+
+        [HttpGet("CrCalculoOperacion_Codigo_Obtener")]
+        public ErrorDto<CrCalculoOperacionCodigoData> CrCalculoOperacion_Codigo_Obtener(int codEmpresa, string cedula, string codigo)
+            => _bl.CrCalculoOperacion_Codigo_Obtener(codEmpresa, cedula, codigo);
+
+        [HttpGet("CrCalculoOperacion_Rangos_Obtener")]
+        public ErrorDto<CrCalculoOperacionRangosData> CrCalculoOperacion_Rangos_Obtener(int codEmpresa, string codigo, decimal monto)
+            => _bl.CrCalculoOperacion_Rangos_Obtener(codEmpresa, codigo, monto);
+
+        [HttpGet("CrCalculoOperacion_Disponibles_Obtener")]
+        public ErrorDto<List<CrCalculoOperacionDisponibleData>> CrCalculoOperacion_Disponibles_Obtener(int codEmpresa, string cedula)
+            => _bl.CrCalculoOperacion_Disponibles_Obtener(codEmpresa, cedula);
+
+        [HttpGet("CrCalculoOperacion_Catalogo_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> CrCalculoOperacion_Catalogo_Obtener(int codEmpresa)
+            => _bl.CrCalculoOperacion_Catalogo_Obtener(codEmpresa);
+    }
+}
