a3951f09dafa86d6d16b7564067f20c0a29ce6cd
Franciny Suárez <150962001+fsuarez23@users.noreply.github.com>
2026-06-03 09:47:11 -0600

    Modelos pendientes v1

---
+++ Galileo_API/Models/ProGrX_Procesos/frmAH_Excedentes_DistribucionModels.cs
@@ -0,0 +1,36 @@
+﻿namespace Galileo.Models.ProGrX_Procesos
+{
+
+    public class AHExcMontoDto
+    {
+        // ================= CAMPOS EDITABLES =================
+        public string periodo { get; set; } = "";
+        public string corte { get; set; } = "";
+        public string tipo { get; set; } = "";
+        public string baseCalculo { get; set; } = "";
+
+        public decimal porcentaje { get; set; }
+        public decimal monto { get; set; }
+        public string justificacion { get; set; } = "";
+
+        // ================= CAMPOS DE LA LISTA (SP) =================
+
+    }
+
+
+    public class AHExcMontoListadoDto
+    {
+        public int id_periodo { get; set; }
+        public string corte { get; set; } = "";
+        public int anio { get; set; }
+        public string mes { get; set; } = "";
+        public decimal monto_proyectado { get; set; }
+        public decimal monto_cargado { get; set; }
+        public decimal monto_real { get; set; }
+        public decimal monto_prorrateado { get; set; }
+        public decimal porc_distribuido { get; set; }
+        public string base_calculo { get; set; } = "";
+        public decimal diferencia { get; set; }
+        public string base_calculo_desc { get; set; } = "";
+    }
+}
