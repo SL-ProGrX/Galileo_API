932302b9635c1de79f4e9e4b74d77fc11a8767b2
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-06-03 07:12:05 -0600

    Update frmTES_TransaccionesModels.cs

--- Galileo_API/Models/ProGrX/Bancos/frmTES_TransaccionesModels.cs
+++ Galileo_API/Models/ProGrX/Bancos/frmTES_TransaccionesModels.cs
@@ -135,6 +135,8 @@ namespace Galileo.Models.TES
         public DateTime creacion_fecha { get; set; }
     }
 
+    /**
+    **Se repite en tes_transacciones -- cambiar nombre o heredar -- de TesTransAsientoDto
     public partial class TesTransAsientoDto
     {
         public string cod_cuenta { get; set; } = string.Empty;
@@ -150,6 +152,7 @@ namespace Galileo.Models.TES
         public decimal tipo_cambio { get; set; }
         public string cod_divisa { get; set; } = string.Empty;
     }
+    **/
 
     public partial  class TesBitacoraDto
     {
