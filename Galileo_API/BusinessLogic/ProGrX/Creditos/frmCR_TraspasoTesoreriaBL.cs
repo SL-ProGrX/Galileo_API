209483d784b739e0bbc645aebb3f22440bad8be3
Franciny Suárez <150962001+fsuarez23@users.noreply.github.com>
2026-06-04 12:58:42 -0600

    Crédito V1-V2

---
+++ Galileo_API/BusinessLogic/ProGrX/Creditos/frmCR_TraspasoTesoreriaBL.cs
@@ -0,0 +1,63 @@
+﻿using Galileo.DataBaseTier.ProGrX.Credito;
+using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Clientes;
+using Galileo.Models.ProGrX.Credito;
+
+namespace Galileo.BusinessLogic.ProGrX.Credito
+{
+    public class FrmCRTraspasoTesoreriaBL
+    {
+        private readonly FrmCRTraspasoTesoreriaDB _db;
+
+        public FrmCRTraspasoTesoreriaBL(IConfiguration config)
+        {
+            _db = new FrmCRTraspasoTesoreriaDB(config);
+        }
+
+        #region remesas
+        #endregion
+
+        #region cargar
+        #endregion
+
+        #region trasladar
+
+        public ErrorDto<List<DropDownListaGenericaModel>> Cr_TraspasoTes_Remesas_Obtener(int CodEmpresa)
+        {
+            return _db.Cr_TraspasoTes_Remesas_Obtener(CodEmpresa);
+        }
+
+        public ErrorDto<List<TokenConsultaModel>> Cr_TraspasoTesToken_Obtener(int CodEmpresa, string usuario)
+        {
+            return _db.Cr_TraspasoTesToken_Obtener(CodEmpresa, usuario);
+        }
+
+        public ErrorDto Cr_TraspasoTesToken_Nuevo(int CodEmpresa, string usuario)
+        {
+            return _db.Cr_TraspasoTesToken_Nuevo(CodEmpresa, usuario);
+        }
+
+        public ErrorDto<List<TraspasoModel>> Cr_TraspasoTesTraslado_Buscar(int CodEmpresa, int cod_remesa)
+        {
+            return _db.Cr_TraspasoTesTraslado_Buscar(CodEmpresa, cod_remesa);
+        }
+
+        #endregion
+
+        #region informes
+        #endregion
+
+        #region reactivaciones
+        #endregion
+
+        #region cambio
+        #endregion
+
+        #region consultas
+        #endregion
+
+        #region aux.giro
+        #endregion
+    }
+}
