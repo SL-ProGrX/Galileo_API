c0572a959d41649ed63548159be3d5ed629867f4
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-06-04 16:47:56 -0600

    Update frmCR_RetencionDeduccionesModels.cs

--- Galileo_API/Models/ProGrX/Creditos/frmCR_RetencionDeduccionesModels.cs
+++ Galileo_API/Models/ProGrX/Creditos/frmCR_RetencionDeduccionesModels.cs
@@ -17,11 +17,11 @@ namespace Galileo_API.Models.ProGrX.Credito
         public List<DropDownListaGenericaModel> tipos { get; set; } = new();
         public string formato_default { get; set; } = "01";
         public string tipo_default { get; set; } = "P";
-        public DateTime? fecha_servidor { get; set; }
+        public DateTime fecha_servidor { get; set; }
         public string proceso_default { get; set; } = string.Empty;
     }
 
-    public class CrRetencionDeduccionesObtenerRequest
+    public class CrRetencionDeduccionesRequestBase
     {
         public string codigo { get; set; } = string.Empty;
         public int? cod_institucion { get; set; }
@@ -31,6 +31,11 @@ namespace Galileo_API.Models.ProGrX.Credito
         public DateTime? fecha_corte { get; set; }
         public string proceso { get; set; } = string.Empty;
         public string usuario { get; set; } = string.Empty;
+    }
+
+    public class CrRetencionDeduccionesObtenerRequest
+        : CrRetencionDeduccionesRequestBase
+    {
     }
 
     public class CrRetencionDeduccionesResultadoData
@@ -41,15 +46,8 @@ namespace Galileo_API.Models.ProGrX.Credito
     }
 
     public class CrRetencionDeduccionesArchivoRequest
+        : CrRetencionDeduccionesRequestBase
     {
-        public string codigo { get; set; } = string.Empty;
-        public int? cod_institucion { get; set; }
-        public string formato { get; set; } = string.Empty;
-        public string tipo { get; set; } = string.Empty;
-        public DateTime? fecha_inicio { get; set; }
-        public DateTime? fecha_corte { get; set; }
-        public string proceso { get; set; } = string.Empty;
-        public string usuario { get; set; } = string.Empty;
     }
 
     public class CrRetencionDeduccionesArchivoData
