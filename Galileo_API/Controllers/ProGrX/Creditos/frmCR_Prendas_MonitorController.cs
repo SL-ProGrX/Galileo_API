a57fe9be547b2da400634fb101ebb08aed8bb884
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-06-21 12:41:08 -0600

    frmCR_Prendas_Monitor

---
+++ Galileo_API/Controllers/ProGrX/Creditos/frmCR_Prendas_MonitorController.cs
@@ -0,0 +1,37 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Creditos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Creditos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCrPrendasMonitorController : ControllerBase
+    {
+        private readonly FrmCrPrendasMonitorBl _bl;
+
+        public FrmCrPrendasMonitorController(IConfiguration config)
+        {
+            _bl = new FrmCrPrendasMonitorBl(config);
+        }
+
+        [HttpGet("CrPrendasMonitor_TiposPrenda_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> CrPrendasMonitor_TiposPrenda_Obtener(int codEmpresa)
+            => _bl.CrPrendasMonitor_TiposPrenda_Obtener(codEmpresa);
+
+        [HttpGet("CrPrendasMonitor_Catalogo_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> CrPrendasMonitor_Catalogo_Obtener(int codEmpresa, string tipo)
+            => _bl.CrPrendasMonitor_Catalogo_Obtener(codEmpresa, tipo);
+
+        [HttpGet("CrPrendasMonitor_EstadosPersona_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> CrPrendasMonitor_EstadosPersona_Obtener(int codEmpresa)
+            => _bl.CrPrendasMonitor_EstadosPersona_Obtener(codEmpresa);
+
+        [HttpGet("CrPrendasMonitor_UnidadesCilindraje_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> CrPrendasMonitor_UnidadesCilindraje_Obtener(int codEmpresa, string tipo)
+            => _bl.CrPrendasMonitor_UnidadesCilindraje_Obtener(codEmpresa, tipo);
+    }
+}
