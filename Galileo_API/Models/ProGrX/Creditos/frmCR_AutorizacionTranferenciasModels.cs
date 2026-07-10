f40c135f94dbdc5b6652fac25ecc1e4fbf21810f
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-07-10 17:24:34 -0600

    nullable

--- Galileo_API/Models/ProGrX/Creditos/frmCR_AutorizacionTranferenciasModels.cs
+++ Galileo_API/Models/ProGrX/Creditos/frmCR_AutorizacionTranferenciasModels.cs
@@ -23,7 +23,7 @@
 
     public class CrAutorizacionTranferenciasOperacionTagRegistrarRequest
     {
-        public int Operacion { get; set; }
+        public int? Operacion { get; set; }
         public string Linea { get; set; } = string.Empty;
         public string Tag { get; set; } = string.Empty;
         public string Usuario { get; set; } = string.Empty;
