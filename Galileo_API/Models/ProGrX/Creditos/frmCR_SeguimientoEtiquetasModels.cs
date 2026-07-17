8db05da35d47a9017db0b6a971d78962463810d5
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-07-16 19:24:31 -0600

    nullable

--- Galileo_API/Models/ProGrX/Creditos/frmCR_SeguimientoEtiquetasModels.cs
+++ Galileo_API/Models/ProGrX/Creditos/frmCR_SeguimientoEtiquetasModels.cs
@@ -15,7 +15,7 @@
 
     public class CrSeguimientoEtiquetasAplicarRequest
     {
-        public int Operacion { get; set; }
+        public int? Operacion { get; set; }
         public string Linea { get; set; } = string.Empty;
         public string Tag { get; set; } = string.Empty;
         public string Usuario { get; set; } = string.Empty;
