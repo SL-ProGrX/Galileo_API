37096994d2e7569216a31d01784b24ca54479dc7
Cristian Campos <ccampos@mpbsystemlogic.com>
2026-07-16 18:20:42 -0600

    frmCR_SeguimientoEtiquetas

---
+++ Galileo_API/Controllers/ProGrX/Creditos/frmCR_SeguimientoEtiquetasController.cs
@@ -0,0 +1,50 @@
+﻿using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX.Creditos;
+using Galileo_API.Models.ProGrX.Creditos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX.Creditos
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FrmCrSeguimientoEtiquetasController : ControllerBase
+    {
+        private readonly FrmCrSeguimientoEtiquetasBl _bl;
+
+        public FrmCrSeguimientoEtiquetasController(IConfiguration config)
+        {
+            _bl = new FrmCrSeguimientoEtiquetasBl(config);
+        }
+
+        [Authorize]
+        [HttpGet("Cr_SeguimientoEtiquetas_Lista_Obtener")]
+        public ErrorDto<List<CrSeguimientoEtiquetasData>> Cr_SeguimientoEtiquetas_Lista_Obtener(int codEmpresa, int idSolicitud)
+        {
+            return _bl.Cr_SeguimientoEtiquetas_Lista_Obtener(codEmpresa, idSolicitud);
+        }
+
+        [Authorize]
+        [HttpGet("Cr_SeguimientoEtiquetas_Etiquetas_Dropdown_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>> Cr_SeguimientoEtiquetas_Etiquetas_Dropdown_Obtener(int codEmpresa, string usuario)
+        {
+            return _bl.Cr_SeguimientoEtiquetas_Etiquetas_Dropdown_Obtener(codEmpresa, usuario);
+        }
+
+        [Authorize]
+        [HttpPost("Cr_SeguimientoEtiquetas_Aplicar")]
+        public ErrorDto Cr_SeguimientoEtiquetas_Aplicar(int codEmpresa, CrSeguimientoEtiquetasAplicarRequest request)
+        {
+            return _bl.Cr_SeguimientoEtiquetas_Aplicar(codEmpresa, request);
+        }
+
+        [Authorize]
+        [HttpGet("Cr_SeguimientoEtiquetas_NotaLargo_Obtener")]
+        public ErrorDto<int> Cr_SeguimientoEtiquetas_NotaLargo_Obtener(int codEmpresa, string tagCodigo)
+        {
+            return _bl.Cr_SeguimientoEtiquetas_NotaLargo_Obtener(codEmpresa, tagCodigo);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Creditos/frmCR_SeguimientoEtiquetasModels.cs
@@ -0,0 +1,25 @@
+﻿namespace Galileo_API.Models.ProGrX.Creditos
+{
+    public class CrSeguimientoEtiquetasData
+    {
+        public short Linea { get; set; }
+        public string Codigo { get; set; } = string.Empty;
+        public int Id_Solicitud { get; set; }
+        public string Tag_Codigo { get; set; } = string.Empty;
+        public string Asignado_A { get; set; } = string.Empty;
+        public DateTime? Registro_Fecha { get; set; }
+        public string Registro_Usuario { get; set; } = string.Empty;
+        public string Notas { get; set; } = string.Empty;
+        public string Etiqueta { get; set; } = string.Empty;
+    }
+
+    public class CrSeguimientoEtiquetasAplicarRequest
+    {
+        public int Operacion { get; set; }
+        public string Linea { get; set; } = string.Empty;
+        public string Tag { get; set; } = string.Empty;
+        public string Usuario { get; set; } = string.Empty;
+        public string Asignado { get; set; } = string.Empty;
+        public string Notas { get; set; } = string.Empty;
+    }
+}
