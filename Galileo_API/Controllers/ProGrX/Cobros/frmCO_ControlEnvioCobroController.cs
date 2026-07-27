2e5cdbc3ccf5316c1cc75e7c3aba16937e568cb5
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-07-27 16:04:57 -0600

    Desarrollo de frmCO_ControlEnvioCobro

---
+++ Galileo_API/Controllers/ProGrX/Cobros/frmCO_ControlEnvioCobroController.cs
@@ -0,0 +1,42 @@
+﻿using Galileo.BusinessLogicTier.ProGrX.Cobros;
+using Galileo.Models;
+using Galileo.Models.ERROR;
+using Galileo.Models.ProGrX.Cobros;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo.Controllers.ProGrX.Cobros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public sealed class FrmCOControlEnvioCobroController : ControllerBase
+    {
+        private readonly FrmCOControlEnvioCobroBL _bl;
+
+        public FrmCOControlEnvioCobroController(IConfiguration config)
+        {
+            _bl = new FrmCOControlEnvioCobroBL(config);
+        }
+        
+        [HttpGet("Co_ControlEnvioCobro_Gestiones_Obtener")]
+        public ErrorDto<List<DropDownListaGenericaModel>>
+            Co_ControlEnvioCobro_Gestiones_Obtener(int codEmpresa)
+        {
+            return _bl.Co_ControlEnvioCobro_Gestiones_Obtener(codEmpresa);
+        }
+
+        [HttpGet("Co_ControlEnvioCobro_Pendientes_Obtener")]
+        public ErrorDto<List<CoControlEnvioCobroPendienteData>>
+            Co_ControlEnvioCobro_Pendientes_Obtener(
+                int codEmpresa,
+                bool todos,
+                string? codGestion = null)
+        {
+            return _bl.Co_ControlEnvioCobro_Pendientes_Obtener(
+                codEmpresa,
+                todos,
+                codGestion);
+        }
+    }
+}
---
+++ Galileo_API/Models/ProGrX/Cobros/frmCO_ControlEnvioCobroModels.cs
@@ -0,0 +1,15 @@
+﻿namespace Galileo.Models.ProGrX.Cobros
+{
+    public sealed class CoControlEnvioCobroPendienteData
+    {
+        public int cod_seg { get; set; } = 0;
+        public DateTime? fecha { get; set; }
+        public string cedula { get; set; } = string.Empty;
+        public string nombre { get; set; } = string.Empty;
+        public decimal monto { get; set; } = 0;
+        public string cod_gestion { get; set; } = string.Empty;
+        public string gestion_x { get; set; } = string.Empty;
+        public string usuario { get; set; } = string.Empty;
+        public string codigo_referencia { get; set; } = string.Empty;
+    }
+}
