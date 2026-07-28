7f3f576ee425d9502d09051e3b94eeca5629a10a
asaenzmpb <asaenz@mpbsystemlogic.com>
2026-07-28 16:51:33 -0600

    update

--- Galileo_API/BusinessLogic/ProGrX_Pasivos/frmCR_APA_LineasBL.cs
+++ Galileo_API/BusinessLogic/ProGrX_Pasivos/frmCR_APA_LineasBL.cs
@@ -6,23 +6,38 @@ namespace Galileo_API.BusinessLogic.ProGrX_Pasivos
 {
     public class FrmCrApaLineasBL
     {
-        private readonly FrmCrApaLineasDB _db;
+        private readonly FrmCrApaLineasDB _Db;
 
-        public FrmCrApaLineasBL(IConfiguration config) => _db = new FrmCrApaLineasDB(config);
+        public FrmCrApaLineasBL(IConfiguration config)
+        {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+            _Db = new FrmCrApaLineasDB(config);
+        }
 
-        public ErrorDto<FrmCrApaLineaCatalogosDto> CR_APA_Lineas_Catalogos_Obtener(int codEmpresa) =>
-            _db.CR_APA_Lineas_Catalogos_Obtener(codEmpresa);
+        public ErrorDto<FrmCrApaLineaCatalogosDto> CR_APA_Lineas_Catalogos_Obtener(int codEmpresa)
+        {
+            return _Db.CR_APA_Lineas_Catalogos_Obtener(codEmpresa);
+        }
 
-        public ErrorDto<List<FrmCrApaLineaCatalogoDto>> CR_APA_Lineas_CentrosCosto_Obtener(int codEmpresa, string cod_unidad) =>
-            _db.CR_APA_Lineas_CentrosCosto_Obtener(codEmpresa, cod_unidad);
+        public ErrorDto<List<FrmCrApaLineaCatalogoDto>> CR_APA_Lineas_CentrosCosto_Obtener(int codEmpresa, string cod_unidad)
+        {
+            return _Db.CR_APA_Lineas_CentrosCosto_Obtener(codEmpresa, cod_unidad);
+        }
 
-        public ErrorDto<List<FrmCrApaLineaGridDto>> CR_APA_Lineas_Consultar(int codEmpresa, FrmCrApaLineaConsultaRequest request) =>
-            _db.CR_APA_Lineas_Consultar(codEmpresa, request);
+        public ErrorDto<List<FrmCrApaLineaGridDto>> CR_APA_Lineas_Consultar(int codEmpresa, FrmCrApaLineaConsultaRequest request)
+        {
+            return _Db.CR_APA_Lineas_Consultar(codEmpresa, request);
+        }
 
-        public ErrorDto<FrmCrApaLineaDatosDto> CR_APA_Lineas_Obtener(int codEmpresa, int cod_linea) =>
-            _db.CR_APA_Lineas_Obtener(codEmpresa, cod_linea);
+        public ErrorDto<FrmCrApaLineaDatosDto> CR_APA_Lineas_Obtener(int codEmpresa, int cod_linea)
+        {
+            return _Db.CR_APA_Lineas_Obtener(codEmpresa, cod_linea);
+        }
 
-        public ErrorDto<FrmCrApaLineaGuardarResultadoDto> CR_APA_Lineas_Guardar(int codEmpresa, FrmCrApaLineaGuardarRequest request) =>
-            _db.CR_APA_Lineas_Guardar(codEmpresa, request);
+        public ErrorDto<FrmCrApaLineaGuardarResultadoDto> CR_APA_Lineas_Guardar(int codEmpresa, FrmCrApaLineaGuardarRequest request)
+        {
+            return _Db.CR_APA_Lineas_Guardar(codEmpresa, request);
+        }
     }
 }
--- Galileo_API/Controllers/ProGrX_Pasivos/frmCR_APA_LineasController.cs
+++ Galileo_API/Controllers/ProGrX_Pasivos/frmCR_APA_LineasController.cs
@@ -8,33 +8,52 @@ namespace Galileo_API.Controllers.ProGrX_Pasivos
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class FrmCrApaLineasController : ControllerBase
     {
-        private readonly FrmCrApaLineasBL _bl;
+        private readonly FrmCrApaLineasBL _BL;
 
-        public FrmCrApaLineasController(IConfiguration config) => _bl = new FrmCrApaLineasBL(config);
+        public FrmCrApaLineasController(IConfiguration config)
+        {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+            _BL = new FrmCrApaLineasBL(config);
+        }
 
+        [Authorize]
         [HttpGet("CR_APA_Lineas_Catalogos_Obtener")]
-        public ErrorDto<FrmCrApaLineaCatalogosDto> CR_APA_Lineas_Catalogos_Obtener(int codEmpresa) =>
-            _bl.CR_APA_Lineas_Catalogos_Obtener(codEmpresa);
+        public ErrorDto<FrmCrApaLineaCatalogosDto> CR_APA_Lineas_Catalogos_Obtener(int codEmpresa)
+        {
+            return _BL.CR_APA_Lineas_Catalogos_Obtener(codEmpresa);
+        }
 
+        [Authorize]
         [HttpGet("CR_APA_Lineas_CentrosCosto_Obtener")]
-        public ErrorDto<List<FrmCrApaLineaCatalogoDto>> CR_APA_Lineas_CentrosCosto_Obtener(int codEmpresa, string cod_unidad) =>
-            _bl.CR_APA_Lineas_CentrosCosto_Obtener(codEmpresa, cod_unidad);
+        public ErrorDto<List<FrmCrApaLineaCatalogoDto>> CR_APA_Lineas_CentrosCosto_Obtener(int codEmpresa, string cod_unidad)
+        {
+            return _BL.CR_APA_Lineas_CentrosCosto_Obtener(codEmpresa, cod_unidad);
+        }
 
+        [Authorize]
         [HttpGet("CR_APA_Lineas_Consultar")]
         public ErrorDto<List<FrmCrApaLineaGridDto>> CR_APA_Lineas_Consultar(
-            int codEmpresa, [FromQuery] FrmCrApaLineaConsultaRequest request) =>
-            _bl.CR_APA_Lineas_Consultar(codEmpresa, request);
+            int codEmpresa, [FromQuery] FrmCrApaLineaConsultaRequest request)
+        {
+            return _BL.CR_APA_Lineas_Consultar(codEmpresa, request);
+        }
 
+        [Authorize]
         [HttpGet("CR_APA_Lineas_Obtener")]
-        public ErrorDto<FrmCrApaLineaDatosDto> CR_APA_Lineas_Obtener(int codEmpresa, int cod_linea) =>
-            _bl.CR_APA_Lineas_Obtener(codEmpresa, cod_linea);
+        public ErrorDto<FrmCrApaLineaDatosDto> CR_APA_Lineas_Obtener(int codEmpresa, int cod_linea)
+        {
+            return _BL.CR_APA_Lineas_Obtener(codEmpresa, cod_linea);
+        }
 
+        [Authorize]
         [HttpPost("CR_APA_Lineas_Guardar")]
         public ErrorDto<FrmCrApaLineaGuardarResultadoDto> CR_APA_Lineas_Guardar(
-            int codEmpresa, FrmCrApaLineaGuardarRequest request) =>
-            _bl.CR_APA_Lineas_Guardar(codEmpresa, request);
+            int codEmpresa, [FromBody] FrmCrApaLineaGuardarRequest request)
+        {
+            return _BL.CR_APA_Lineas_Guardar(codEmpresa, request);
+        }
     }
 }
