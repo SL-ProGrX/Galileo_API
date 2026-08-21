cba85387e55313e64382328c8d48ab2027b1a3b8
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-08-21 17:49:39 -0600

    Desarrollo de frmCC_Estado_Cuenta_Mail

---
+++ Galileo_API/BusinessLogic/ProGrX/General/frmCC_Estado_Cuenta_MailBL.cs
@@ -0,0 +1,36 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX.General;
+using Galileo_API.Models.ProGrX.General;
+
+namespace Galileo_API.BusinessLogic.ProGrX.General
+{
+    public sealed class FrmCcEstadoCuentaMailBl
+    {
+        private readonly FrmCcEstadoCuentaMailDb _db;
+
+        public FrmCcEstadoCuentaMailBl(
+            IConfiguration config)
+        {
+            _db = new FrmCcEstadoCuentaMailDb(config);
+        }
+
+        public ErrorDto<CcEstadoCuentaMailInicialData>
+            CC_Estado_Cuenta_Mail_Inicializar(
+                int codEmpresa,
+                string cedula)
+        {
+            return _db.CC_Estado_Cuenta_Mail_Inicializar(
+                codEmpresa,
+                cedula);
+        }
+
+        public ErrorDto CC_Estado_Cuenta_Mail_Enviar(
+            int codEmpresa,
+            CcEstadoCuentaMailEnviarRequest? request)
+        {
+            return _db.CC_Estado_Cuenta_Mail_Enviar(
+                codEmpresa,
+                request);
+        }
+    }
+}
