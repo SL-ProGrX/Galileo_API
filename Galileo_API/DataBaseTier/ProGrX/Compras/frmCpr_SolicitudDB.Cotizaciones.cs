50e59e8abbb48cb6cf8979c9ca754eeab1cca4db
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-08-24 13:21:31 -0600

    Create frmCpr_SolicitudDB.Cotizaciones.cs

---
+++ Galileo_API/DataBaseTier/ProGrX/Compras/frmCpr_SolicitudDB.Cotizaciones.cs
@@ -0,0 +1,61 @@
+using Dapper;
+using Galileo.Models.CPR;
+using System.Data;
+
+namespace Galileo.DataBaseTier
+{
+    public partial class FrmCprSolicitudDB
+    {
+        private static void CprSolicitudCotizacionBs_UnidadesAsignar(
+            IDbConnection conn,
+            List<CprSolicitudCotizacionPrvBs> cotizaciones)
+        {
+            var productos = CprSolicitudCotizacionBs_ProductosObtener(cotizaciones);
+            if (productos.Count == 0)
+            {
+                return;
+            }
+
+            const string qUnidades = "SELECT COD_PRODUCTO, COD_UNIDAD FROM PV_PRODUCTOS WHERE COD_PRODUCTO IN @Productos;";
+            var map = conn.Query<(string COD_PRODUCTO, string COD_UNIDAD)>(qUnidades, new { Productos = productos })
+                .ToDictionary(x => x.COD_PRODUCTO, x => x.COD_UNIDAD);
+
+            foreach (var item in cotizaciones)
+            {
+                CprSolicitudCotizacionBs_UnidadAsignar(item, map);
+            }
+        }
+
+        private static List<string> CprSolicitudCotizacionBs_ProductosObtener(
+            IEnumerable<CprSolicitudCotizacionPrvBs> cotizaciones)
+        {
+            var productos = new HashSet<string>();
+            foreach (var item in cotizaciones)
+            {
+                var codProducto = item.cod_producto;
+                if (!string.IsNullOrWhiteSpace(codProducto))
+                {
+                    productos.Add(codProducto);
+                }
+            }
+
+            return productos.ToList();
+        }
+
+        private static void CprSolicitudCotizacionBs_UnidadAsignar(
+            CprSolicitudCotizacionPrvBs item,
+            IReadOnlyDictionary<string, string> unidades)
+        {
+            var codProducto = item.cod_producto;
+            if (string.IsNullOrWhiteSpace(codProducto))
+            {
+                return;
+            }
+
+            if (unidades.TryGetValue(codProducto, out string? unidad))
+            {
+                item.unidad = unidad;
+            }
+        }
+    }
+}
