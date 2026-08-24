57f4e8d620ad9fe59cd5ed2f626e1db2fb273ff7
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-08-24 11:35:41 -0600

    Desarrollo de frmCierreMensualASE

---
+++ Galileo_API/BusinessLogic/ProGrX_Conciliacion/frmCierreMensualASEBL.cs
@@ -0,0 +1,26 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.DataBaseTier.ProGrX_Conciliacion;
+
+namespace Galileo_API.BusinessLogic.ProGrX_Conciliacion
+{
+    public sealed class FrmCierreMensualAseBl
+    {
+        private readonly FrmCierreMensualAseDb _db;
+
+        public FrmCierreMensualAseBl(
+            IConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+            _db = new FrmCierreMensualAseDb(config);
+        }
+
+        public ErrorDto
+            Conciliacion_CierreMensualASE_Cierre_Ejecutar(
+                int codEmpresa, string usuario)
+        {
+            return _db
+                .Conciliacion_CierreMensualASE_Cierre_Ejecutar(
+                    codEmpresa, usuario);
+        }
+    }
+}
---
+++ Galileo_API/Controllers/ProGrX_Conciliacion/frmCierreMensualASEController.cs
@@ -0,0 +1,35 @@
+﻿using Galileo.Models.ERROR;
+using Galileo_API.BusinessLogic.ProGrX_Conciliacion;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galileo_API.Controllers.ProGrX_Conciliacion
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public sealed class FrmCierreMensualAseController :
+        ControllerBase
+    {
+        private readonly FrmCierreMensualAseBl _bl;
+
+        public FrmCierreMensualAseController(
+            IConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+            _bl = new FrmCierreMensualAseBl(config);
+        }
+
+        [HttpPost(
+            "Conciliacion_CierreMensualASE_Cierre_Ejecutar")]
+        public ErrorDto
+            Conciliacion_CierreMensualASE_Cierre_Ejecutar(
+                int codEmpresa, string usuario)
+        {
+            return _bl
+                .Conciliacion_CierreMensualASE_Cierre_Ejecutar(
+                    codEmpresa,
+                    usuario);
+        }
+    }
+}
