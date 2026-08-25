76203e910a9571c9f960cd3788edb67e6e160027
fbogantes-systemlogic <fbogantes@mpbsystemlogic.com>
2026-08-25 15:37:27 -0600

    Migración frmCC_AutorizaSolicitudes

--- Galileo_API/Models/ProGrX/General/frmCC_AutorizaSolicitudesModels.cs
+++ Galileo_API/Models/ProGrX/General/frmCC_AutorizaSolicitudesModels.cs
@@ -2,53 +2,52 @@ namespace Galileo.Models.GEN
 {
     public class AutorizaSolicitudesCreditoData
     {
-        public int id_solicitud { get; set; }
+        public int id_solicitud { get; set; } = 0;
         public string codigo { get; set; } = string.Empty;
         public string cedula { get; set; } = string.Empty;
         public string nombre { get; set; } = string.Empty;
-        public decimal monto_girado { get; set; }
+        public decimal monto_girado { get; set; } = 0;
     }
 
     public class AutorizaSolicitudesFondosData
     {
-        public int consec { get; set; }
+        public int consec { get; set; } = 0;
         public string cedula { get; set; } = string.Empty;
         public string nombre { get; set; } = string.Empty;
         public string cod_plan { get; set; } = string.Empty;
-        public int cod_contrato { get; set; }
-        public decimal total_girar { get; set; }
+        public int cod_contrato { get; set; } = 0;
+        public decimal total_girar { get; set; } = 0;
     }
 
     public class AutorizaSolicitudesLiquidacionData
     {
-        public int consec { get; set; }
+        public int consec { get; set; } = 0;
         public string cedula { get; set; } = string.Empty;
         public string nombre { get; set; } = string.Empty;
-        public decimal tneto { get; set; }
+        public decimal tneto { get; set; } = 0;
         public string tipo { get; set; } = string.Empty;
     }
 
     public class AutorizaSolicitudesBeneficiosData
     {
-        public int consec { get; set; }
+        public int consec { get; set; } = 0;
         public string cedula { get; set; } = string.Empty;
         public string nombre { get; set; } = string.Empty;
-        public decimal monto { get; set; }
+        public decimal monto { get; set; } = 0;
         public string cod_beneficio { get; set; } = string.Empty;
     }
 
     public class AutorizaSolicitudesHipotecarioData
     {
-        public int codigodesembolso { get; set; }
-        public int numerooperacion { get; set; }
+        public int codigodesembolso { get; set; } = 0;
+        public int numerooperacion { get; set; } = 0;
         public string beneficiario { get; set; } = string.Empty;
-        public decimal monto { get; set; }
-        public DateTime registrofecha { get; set; }
+        public decimal monto { get; set; } = 0;
+        public DateTime? registrofecha { get; set; }
         public string registrousuario { get; set; } = string.Empty;
         public string cedula { get; set; } = string.Empty;
         public string nombre { get; set; } = string.Empty;
         public string codigo { get; set; } = string.Empty;
-        public string tes_supervision_fecha { get; set; } = string.Empty;
-
+        public DateTime? tes_supervision_fecha { get; set; }
     }
 }
