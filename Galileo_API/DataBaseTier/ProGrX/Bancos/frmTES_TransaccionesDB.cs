c8b4e74365b26dff44b14fd7f36261e7b84ff026
David Gutierrez M <dgutierrez@mpbsystemlogic.com>
2026-09-09 12:35:12 -0600

    Update frmTES_TransaccionesDB.cs

--- Galileo_API/DataBaseTier/ProGrX/Bancos/frmTES_TransaccionesDB.cs
+++ Galileo_API/DataBaseTier/ProGrX/Bancos/frmTES_TransaccionesDB.cs
@@ -784,6 +784,7 @@ namespace Galileo_API.DataBaseTier.ProGrX.Bancos
             t.ndocumento = "";
 
             using var connection = DbHelper.OpenConnection(_portalDB, CodEmpresa);
+
             var vFecha = mTesFunciones.TES_EmisionDocumentos_FechaEmisionResolver(connection, t.documento_banco);
 
             var emision = mTesoreria.sbTesEmitirDocumento(CodEmpresa, usuario, vModulo, t.nsolicitud, t.ndocumento, null);
